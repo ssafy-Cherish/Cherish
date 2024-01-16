c344f0935daa2144bbeff99f039b833c8780dbda
이승준 <tmdwns7809@gmail.com>
2024-01-16 18:02:54 +0900

    Feat : 백엔드 프로젝트 생성
    이슈 코드 : []

---
+++ backend/cherish/src/main/java/com/ssafy/cherish/CherishApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cherish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherishApplication.class, args);
+	}
+
+}
