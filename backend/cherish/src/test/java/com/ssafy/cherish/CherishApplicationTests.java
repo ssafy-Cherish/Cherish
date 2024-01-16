842f370935505796081e984b5725f9e3b687d0be
이승준 <tmdwns7800@naver.com>
2024-01-16 18:02:54 +0900

    Feat : 백엔드 프로젝트 생성
    이슈 코드 : []

---
+++ backend/cherish/src/test/java/com/ssafy/cherish/CherishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cherish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
