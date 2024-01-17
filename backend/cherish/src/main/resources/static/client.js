9719652c77a3086c3db6455c6e71d5ca6834fd40
이승준 <tmdwns7800@naver.com>
2024-01-17 09:24:12 +0900

    Feat : WebRTC 테스트 추가
    이슈 코드 : []

---
+++ backend/cherish/src/main/resources/static/client.js
@@ -0,0 +1,144 @@
+var camera = document.getElementById("camera");
+var peer = document.getElementById("peer");
+
+window.onload = function () {
+    // 사용자 카메라 연결
+    const constraints = {
+        video : {
+            frameRate : {
+                ideal : 5,
+                max : 10
+            },
+            width : 72,
+            height : 72,
+            facingMode : "user"
+        }
+    };
+    navigator.mediaDevices.getUserMedia(constraints).
+        then(function (stream) { 
+            camera.srcObject = stream;
+        })
+        .catch(function (err) { /* handle the error */ });
+}
+
+//connecting to our signaling server 
+var conn = new WebSocket('ws://192.168.100.56:8080/socket');
+
+conn.onopen = function() {
+    console.log("Connected to the signaling server");
+    initialize();
+};
+
+conn.onmessage = function(msg) {
+    console.log("Got message", msg.data);
+    var content = JSON.parse(msg.data);
+    var data = content.data;
+    switch (content.event) {
+    // when somebody wants to call us
+    case "offer":
+        handleOffer(data);
+        break;
+    case "answer":
+        handleAnswer(data);
+        break;
+    // when a remote peer sends an ice candidate to us
+    case "candidate":
+        handleCandidate(data);
+        break;
+    default:
+        break;
+    }
+};
+
+function send(message) {
+    conn.send(JSON.stringify(message));
+}
+
+var peerConnection;
+var dataChannel;
+var input = document.getElementById("messageInput");
+
+function initialize() {
+    var configuration = null;
+
+    peerConnection = new RTCPeerConnection(configuration);
+
+    // Setup ice handling
+    peerConnection.onicecandidate = function(event) {
+        if (event.candidate) {
+            send({
+                event : "candidate",
+                data : event.candidate
+            });
+        }
+    };
+
+    // creating data channel
+    dataChannel = peerConnection.createDataChannel("dataChannel", {
+        reliable : true
+    });
+
+    dataChannel.onerror = function(error) {
+        console.log("Error occured on datachannel:", error);
+    };
+
+    // when we receive a message from the other peer, printing it on the console
+    dataChannel.onmessage = function(event) {
+        console.log("message:", event.data);
+    };
+
+    dataChannel.onclose = function() {
+        console.log("data channel is closed");
+    };
+  
+  	peerConnection.ondatachannel = function (event) {
+        dataChannel = event.channel;
+  	};
+
+    peerConnection.onaddstream = function(event) {
+        peer.srcObject = event.stream;
+    };
+}
+
+function createOffer() {
+    peerConnection.createOffer(function(offer) {
+        send({
+            event : "offer",
+            data : offer
+        });
+        peerConnection.setLocalDescription(offer);
+    }, function(error) {
+        alert("Error creating an offer");
+    });
+}
+
+function handleOffer(offer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    // create and send an answer to an offer
+    peerConnection.createAnswer(function(answer) {
+        peerConnection.setLocalDescription(answer);
+        send({
+            event : "answer",
+            data : answer
+        });
+    }, function(error) {
+        alert("Error creating an answer");
+    });
+
+};
+
+function handleCandidate(candidate) {
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+function handleAnswer(answer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    console.log("connection established successfully!!");
+    peerConnection.addStream(camera.srcObject);
+};
+
+function sendMessage() {
+    dataChannel.send(input.value);
+    input.value = "";
+}
