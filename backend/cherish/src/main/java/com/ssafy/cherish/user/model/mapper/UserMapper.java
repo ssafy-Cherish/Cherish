00f7f8e85f377b99e1f3b4a6308df64ae7b475e2
unknown <1025jy@naver.com>
2024-01-25 09:47:55 +0900

    Feat : user 회원가입 추가
    이슈 코드 : [S10P12D103]

--- backend/cherish/src/main/java/com/ssafy/cherish/user/model/mapper/UserMapper.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/user/model/mapper/UserMapper.java
@@ -1,2 +1,18 @@
-package com.ssafy.cherish.user.model.mapper;public interface UserMapper {
+package com.ssafy.cherish.user.model.mapper;
+
+
+import com.ssafy.cherish.user.model.dto.CoupleDto;
+import com.ssafy.cherish.user.model.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.sql.SQLException;
+
+@Mapper
+public interface UserMapper {
+
+    void firstJoin (UserDto userDto) throws SQLException;
+    void secondJoin (UserDto userDto) throws SQLException;
+    void coupleFirstJoin (CoupleDto coupleDto) throws SQLException;
+    void coupleSecondJoin (CoupleDto coupleDto) throws SQLException;
+
 }
--- backend/cherish/src/main/java/com/ssafy/cherish/user/model/service/UserServiceImpl.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/user/model/service/UserServiceImpl.java
@@ -1,2 +1,39 @@
-package com.ssafy.cherish.user.model.service;public class UserServiceImpl {
+package com.ssafy.cherish.user.model.service;
+
+import com.ssafy.cherish.user.model.dto.CoupleDto;
+import com.ssafy.cherish.user.model.dto.UserDto;
+import com.ssafy.cherish.user.model.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public UserServiceImpl (UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public void firstJoin (UserDto userDto) throws Exception {
+        userMapper.firstJoin(userDto);
+    }
+
+    @Override
+    public void secondJoin (UserDto userDto) throws Exception {
+        userMapper.secondJoin(userDto);
+    }
+
+    @Override
+    public void coupleFirstJoin(CoupleDto coupleDto) throws Exception {
+        userMapper.coupleFirstJoin(coupleDto);
+    }
+
+    @Override
+    public void coupleSecondJoin(CoupleDto coupleDto) throws Exception {
+        userMapper.coupleSecondJoin(coupleDto);
+    }
+
+
 }
