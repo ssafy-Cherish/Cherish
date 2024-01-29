3a33d318c7a8337fb2b1d51997f507cc389b4a53
전찬혁 <cksgur5530@gmail.com>
2024-01-29 16:32:51 +0900

    Feat : Vite 앱 Docker 설정 추가 및 디렉토리 구조 설계
    이슈 코드 : [S10P12D103-171]
    
    - vite.config.js 변경
      - Docker setting 내용 추가
    - 디렉토리 별 txt 파일 추가

--- frontend/cherish/vite.config.js
+++ frontend/cherish/vite.config.js
@@ -1,7 +1,17 @@
 import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
+  base: "./",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
   plugins: [million.vite({ auto: true }), react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  assetsInclude: ["**/*.jpg"],
 });
