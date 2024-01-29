74323e72dbb7f33fbe0d0060d24c4f5dc9e2d4ee
yeaaaaahhhhh <k210029@naver.com>
2024-01-29 10:26:03 +0900

    Feat : saveClip 클립 저장 Postmapping API 구현
    이슈 코드 : [S10P12D103-120]
    
    - 파일을 입력받을 형태를 결정하지 못해 현재는 MultiPartFile로 입력받음
    - cherish_video/meetingId/clipId/ 디렉토리에 저장되는 구조

---
+++ backend/cherish/src/main/java/com/ssafy/cherish/clip/controller/ClipController.java
@@ -0,0 +1,49 @@
+package com.ssafy.cherish.clip.controller;
+
+import com.ssafy.cherish.clip.model.dto.ClipDto;
+import com.ssafy.cherish.clip.model.service.ClipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/clip")
+@Slf4j
+public class ClipController {
+    @Autowired
+    private ClipService clipService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "클립 저장", description = "키워드가 포함된 두 명의 동영상 저장하기위해 파일 두개 입력받음")
+    public ResponseEntity saveClip(
+            @RequestPart("clip1") MultipartFile clip1,
+            @RequestPart("clip2") MultipartFile clip2,
+            @Parameter(name = "클립 파일 저장에 필요한 정보 map", description = "user1,user2,meeting_id,keyword")
+            @RequestParam Map<String, Object> map
+    ) {
+        log.debug("클립 입력 : {}", map.toString());
+        try {
+            int res=clipService.saveClip(clip1, clip2, map);
+            if(res==1) {
+                return new ResponseEntity(HttpStatus.CREATED);
+            }
+            else {
+                return new ResponseEntity(HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
