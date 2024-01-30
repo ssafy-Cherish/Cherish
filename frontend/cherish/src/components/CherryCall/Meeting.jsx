8cf53991f14075a0c5abde0a3ba59145976be9a1
양원석 <diddnjstjr11@naver.com>
2024-01-30 18:01:46 +0900

    Feat: 미팅 페이지 작성1
    - 미팅 페이지 요소 작성
    - WebRTC 적용하기
    
    이슈 코드 : []

--- frontend/cherish/src/components/CherryCall/Meeting.jsx
+++ frontend/cherish/src/components/CherryCall/Meeting.jsx
@@ -1,18 +1,22 @@
 import { useRef, useState, useEffect } from "react";
-import classes from './Meeting.module.css'
+
+var setStateGotMediaStream;
+var localMediaStream = new MediaStream();
+var remoteMediaStream = new MediaStream();
+var localVideoObj = new MediaStream();
+var remoteVideoObj = new MediaStream();
+
+var myCamState;
+var setMyCamState;
+var myMicState;
+var setMyMicState;
 
 
-var localStream;
-var remoteStream;
+var conn;
+var peerConnection;
+var dataChannel;
 
-var mediaRecorder;
-var recordedChunks = [];
-var camstate;
-
-var changeChatting;
-
-window.onload = function () {
-  // 사용자 카메라 연결
+const getLocalMediaStream = function () {
   const constraints = {
     video: {
       frameRate: {
@@ -29,94 +33,50 @@ window.onload = function () {
       sampleRate: 44100, // 샘플레이트 설정 (예: 44100Hz)
     },
   };
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function (stream) {
-      localStream = stream;
-      console.log("got stream");
-      mediaRecorder = new MediaRecorder(stream);
-
-      mediaRecorder.ondataavailable = function (event) {
-        if (event.data.size > 0) {
-          console.log("mediaRecorder.ondataavailable");
-          console.log(mediaRecorder.ondataavailable);
-          recordedChunks.push(event.data);
-        }
-      };
-      mediaRecorder.onstop = function (event) {
-        console.log(recordedChunks);
-        let blob = new Blob(recordedChunks, {
-          type: "video/webm",
-        });
-        let url = URL.createObjectURL(blob);
-
-        // 예를 들어, 비디오를 <video> 엘리먼트에 로드할 수 있습니다.
-        let video = document.getElementById("record");
-        video.src = url;
-        video.play();
-      };
-    })
-    .catch(function (err) {
-      console.log(err);
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    stream.getTracks().forEach((track) => {
+      localMediaStream.addTrack(track);
     });
+    setStateGotMediaStream(true);
+    console.log("get Media");
+    console.log(localMediaStream.getTracks());
+    localVideoObj.addTrack(localMediaStream.getAudioTracks()[0])
+    setMyMic
+    localVideoObj.addTrack(localMediaStream.getVideoTracks()[0])
+    setMyCamState(true)
+    document.getElementById("ready-cam").srcObject = localVideoObj;
+  });
 };
 
-//connecting to our signaling server
-// 서버 주소로 변경해야 됨
-var conn = new WebSocket("ws://localhost:8080/socket");
+const setConn = function () {
+  conn = new WebSocket("ws://192.168.100.58:8080/socket");
 
-conn.onopen = function () {
-  console.log("Connected to the signaling server");
-  initialize();
+  conn.onopen = function () {
+    console.log("Connected to the signaling server");
+    initialize();
+  };
+
+  conn.onmessage = function (msg) {
+    console.log("Got message", msg.data);
+    var content = JSON.parse(msg.data);
+    var data = content.data;
+    switch (content.event) {
+      // when somebody wants to call us
+      case "offer":
+        handleOffer(data);
+        break;
+      case "answer":
+        handleAnswer(data);
+        break;
+      // when a remote peer sends an ice candidate to us
+      case "candidate":
+        handleCandidate(data);
+        break;
+      default:
+        break;
+    }
+  };
 };
-
-conn.onmessage = function (msg) {
-  console.log("Got message", msg.data);
-  var content = JSON.parse(msg.data);
-  var data = content.data;
-  switch (content.event) {
-    // when somebody wants to call us
-    case "offer":
-      handleOffer(data);
-      break;
-    case "answer":
-      handleAnswer(data);
-      break;
-    // when a remote peer sends an ice candidate to us
-    case "candidate":
-      handleCandidate(data);
-      break;
-    case "stopCamera":
-      handleStopCamera();
-      break;
-    case "startCamera":
-      handleStartCamera();
-      break;
-    case "requestCameraState":
-      send({
-        event: "responseCameraState",
-        data: camstate,
-      });
-      break;
-    case "responseCameraState":
-      if (data) {
-        document.getElementById("peer").srcObject = remoteStream;
-      } else {
-        document.getElementById("peer").srcObject = null;
-      }
-      break;
-    default:
-      break;
-  }
-};
-
-function send(message) {
-  conn.send(JSON.stringify(message));
-}
-
-var peerConnection;
-var dataChannel;
-var input = document.getElementById("messageInput");
 
 function initialize() {
   var configuration = null;
@@ -144,13 +104,37 @@ function initialize() {
 
   // when we receive a message from the other peer, printing it on the console
   dataChannel.onmessage = function (event) {
-    console.log("message:", event.data);
-    changeChatting((prev) => {
-      return [event.data, ...prev];
-    });
+    const msg = JSON.parse(event.data);
+    console.log(msg);
+    switch (msg.cmd) {
+      case "request peer cam state":
+        sendMessage(
+          JSON.stringify({
+            cmd: "responce peer cam state",
+            data: myCamState,
+          })
+        );
+        break;
+
+      case "responce peer cam state":
+        remoteVideoObj = msg.data ? remoteMediaStream : null;
+        document.getElementById("peer").srcObject = remoteVideoObj;
+        break;
+
+      // case "send chatting massage":
+      //   changeChatting((prev) => {
+      //     return [msg.data, ...prev];
+      //   });
+      //   break;
+      default:
+        break;
+    }
   };
 
   dataChannel.onclose = function () {
+    peerConnection.close();
+    // changePeerConnectionConnected(false);
+    initialize();
     console.log("data channel is closed");
   };
 
@@ -158,26 +142,21 @@ function initialize() {
     dataChannel = event.channel;
   };
 
-  peerConnection.onaddstream = function (event) {
-    remoteStream = event.stream;
-    document.getElementById("peer").srcObject = remoteStream;
-    requestCameraState();
-  };
-}
+  peerConnection.ontrack = function (event) {
+    console.log("ontrack");
+    remoteMediaStream.addTrack(event.track);
 
-function createOffer() {
-  peerConnection.createOffer(
-    function (offer) {
-      send({
-        event: "offer",
-        data: offer,
-      });
-      peerConnection.setLocalDescription(offer);
-    },
-    function (error) {
-      alert("Error creating an offer");
+    if (remoteMediaStream.getTracks().length === 2) {
+      // setMediaRecorder(0, mediaStream, remoteMediaStream);
+      // setMediaRecorder(1, mediaStream, remoteMediaStream);
+      // recordStart();
     }
-  );
+    sendMessage(
+      JSON.stringify({
+        cmd: "request peer cam state",
+      })
+    );
+  };
 }
 
 function handleOffer(offer) {
@@ -192,11 +171,16 @@ function handleOffer(offer) {
         data: answer,
       });
     },
-    function (error) {
+    function () {
       alert("Error creating an answer");
     }
   );
-  peerConnection.addStream(localStream);
+  if (peerConnection.getSenders().length === 0) {
+    localMediaStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track);
+    });
+  }
+  //   peerConnection.addStream(mediaStream);
 }
 
 function handleCandidate(candidate) {
@@ -206,103 +190,128 @@ function handleCandidate(candidate) {
 function handleAnswer(answer) {
   peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
   console.log("connection established successfully!!");
-  peerConnection.addStream(localStream);
 }
+////
 
 function sendMessage(msg) {
   dataChannel.send(msg);
 }
 
-function record() {
-  console.log("녹화 시작");
-  // 녹화 시작
-  mediaRecorder.start(1000);
+function send(message) {
+  conn.send(JSON.stringify(message));
 }
-
-function recordStop() {
-  console.log("녹화 중지");
-  mediaRecorder.stop();
-}
-
-function stopCamera() {
-  document.getElementById("camera").srcObject = null;
-  send({
-    event: "stopCamera",
-    data: "",
-  });
-}
-
-function handleStopCamera() {
-  console.log("handleStopCamera");
-  document.getElementById("peer").srcObject = null;
-}
-
-function startCamera() {
-  document.getElementById("camera").srcObject = localStream;
-  send({
-    event: "startCamera",
-    data: "",
-  });
-}
-
-function handleStartCamera() {
-  console.log("handleStartCamera");
-  document.getElementById("peer").srcObject = remoteStream;
-}
-
-function requestCameraState() {
-  send({
-    event: "requestCameraState",
-    data: "",
-  });
-}
-
-//test
 
 ////////
 
 function Meeting() {
-  const [cameraState, setCameraState] = useState(false);
-  camstate = cameraState;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [gotMediaStream, setGotMediaStream] = useState(false);
+  
+  const [myCam, setMyCam] = useState(false);
+  const [myMic, setMyMic] = useState(false);
 
-  const [chatting, setChatting] = useState([]);
-  changeChatting = setChatting;
   const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    // document.getElementById("offerbutton").onclick = () => {
-    //   createOffer();
-    // };
-    // document.getElementById("recordbutton").onclick = () => {
-    //   record();
-    // };
-    // document.getElementById("recordstopbutton").onclick = () => {
-    //   recordStop();
-    // };
+  const [chatHistory, setChatHistory] = useState([]);
 
-    // document.getElementById("camerabutton").onclick = () => {
-    //   setCameraState((prev) => {
-    //     if (prev) {
-    //       stopCamera();
-    //     } else {
-    //       startCamera();
-    //     }
-    //     camstate = !prev;
-    //     return !prev;
-    //   });
-    // };
+  useEffect(() => {
+    myCamState = myCam;
+    setMyCamState = setMyCam;
+
+    myMicState = myMic;
+    setMyMicState = setMyMic;
+
+    getLocalMediaStream();
+    setStateGotMediaStream = setGotMediaStream;
   }, []);
 
   return (
-    <div className='grid grid-cols-12 gap-5 h-screen justify-center'>
-      <div className="grid grid-rows-12 col-span-8" >
-        <div className="row-start-2 row-end-12 border-2">
-          영상화면
+    <div className="h-full w-full flex flex-row contents-center">
+      <div className="w-9/12 flex flex-col justify-center">
+        <div className="h-3/4 m-2 rounded-2xl flex flex-col-reverse">
+          <div className="h-14 bg-pink rounded-b-2xl flex flex-row justify-between">
+            <div className="border-2 m-2 w-1/6"></div>
+            <button className="border-2 m-2 w-14 rounded-2xl"></button>
+          </div>
+          {!isModalOpen && (
+            <video src="" className="h-full bg-slate-700 rounded-t-2xl"></video>
+          )}
+          {isModalOpen && (
+            <div className="h-full bg-slate-700 flex flex-col justify-center items-center rounded-t-2xl">
+              <div className="h-5/6 w-1/2 mt-5 rounded-2xl bg-pink flex flex-col justify-center items-center">
+                <div className="h-1/3 w-full flex flex-col justify-center text-center font-extrabold text-xl">
+                  체리콜을 시작할까요?
+                </div>
+                <div className="h-full w-5/6  flex flex-col-reverse justify-center">
+                  <div className="h-14 bg-white rounded-b-2xl flex flex-row justify-center">
+                    <button className="w-10 my-2 mx-5 border-2"></button>
+                    <button className="w-10 my-2 mx-5 border-2"></button>
+                  </div>
+                  <video
+                    id="ready-cam"
+                    autoPlay
+                    playsInline
+                    className={myCam?"h-full rounded-t-2xl":"h-full rounded-t-2xl bg-slate-700"}
+                  ></video>
+                </div>
+                <div className="h-1/4 w-5/6 flex flex-row justify-between items-center">
+                  <button
+                    className={
+                      gotMediaStream
+                        ? "px-5 h-14 bg-skyblue rounded-2xl font-extrabold text-xl"
+                        : "px-5 h-14 bg-zinc-400 rounded-2xl font-extrabold text-xl"
+                    }
+                    disabled={!gotMediaStream}
+                  >
+                    알림보내기
+                  </button>
+                  <button
+                    className={
+                      gotMediaStream
+                        ? "px-5 h-14 bg-skyblue rounded-2xl font-extrabold text-xl"
+                        : "px-5 h-14 bg-zinc-400 rounded-2xl font-extrabold text-xl"
+                    }
+                    disabled={!gotMediaStream}
+                    onClick={() => {
+                      setIsModalOpen(false);
+                    }}
+                  >
+                    입장
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div className="grid grid-rows-12 col-span-3">
-        <div className="row-start-2 row-end-12 border-2">
-          채팅화면
+      <div className="w-3/12 flex flex-col justify-center mr-5">
+        <div className="bg-pink h-3/4 m-2 rounded-2xl flex flex-col">
+          <div className="bg-white mx-4 mt-4 mb-2 rounded-t-2xl h-16">
+            <button className="w-[50%] h-full font-extrabold text-xl">
+              체리톡
+            </button>
+            <button className="w-[50%] h-full font-extrabold text-xl">
+              클립
+            </button>
+          </div>
+          <div className="bg-white mx-4 mb-4 rounded-b-2xl h-full"></div>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              console.log(message);
+              setMessage("");
+            }}
+            className="mx-4 mb-4 rounded-2xl h-12 flex flex-row"
+          >
+            <input
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+              value={message}
+              className="bg-white w-full rounded-2xl"
+            ></input>
+            <button className=" border-2 ml-4 w-12"></button>
+          </form>
         </div>
       </div>
     </div>
