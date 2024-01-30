015647c06105926b99cb0362048e3f8fc06ae0e1
김예현 <qkdo12@naver.com>
2024-01-30 09:14:48 +0900

    Chore : 문법, Swagger description 등 세세한 수정사항 적용
    이슈 코드 : []

--- backend/cherish/src/main/java/com/ssafy/cherish/memo/model/dto/MemoDto.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/memo/model/dto/MemoDto.java
@@ -8,7 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Schema(name = "메모", description = "다이어리 매일 입력할 수 있는 메모")
+@Schema(name = "메모", description = "다이어리 내에서 일별로 입력되는 메모")
 public class MemoDto {
     private int id;
     private int coupleId;
