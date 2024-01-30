19595f1296b9b31d29ae1a14df9df59b7a4632ff
전찬혁 <cksgur5530@gmail.com>
2024-01-30 22:57:56 +0900

    Fix : 이미지 형식으로 변경
    이슈 코드 : [S10P12D103-158]
    
    - assets/Month에 월마다 사용할  svg 파일 추가
      - 하나의 공통 이미지를 통해 구현하려고 했는데 실패했습니다.
      - 그래서 이미지를 여러 개 사용하는 것으로 전환했씁니다.
      - 장점 : 코드 짧아짐, 이전 커밋에서 화면 비율 조정시 망가지는 문제
        부분 해결
      - 단점 : import를 많이 함
    - 애니메이션 추가
      - 연도 이동 시 월간 이미지가 사라졌다가 나타나게 변경
    - 이동 방식 변경
      - 기존의 Link에서 navigate로 변경

--- frontend/cherish/src/pages/DiaryPages/DiaryMonthlyPage.jsx
+++ frontend/cherish/src/pages/DiaryPages/DiaryMonthlyPage.jsx
@@ -1,14 +1,14 @@
 import Modal from "../../components/Diary/Modal";
-import monthlyImg from "../../assets/DiaryMonthlyPage.svg";
+// import monthlyImg from "../../assets/DiaryMonthlyPage.svg";
 
 const DiaryMonthlyPage = () => {
-  return (
-    <Modal img={monthlyImg} z={1} height="90vh" width="auto">
-      <div style={{ margin: "1rem 30%" }}>
-        <h1>monthlyImg</h1>
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal z={1} height="90vh" width="70vw">
+			<div style={{ margin: "1rem 30%" }}>
+				<h1>monthlyImg</h1>
+			</div>
+		</Modal>
+	);
 };
 
 export default DiaryMonthlyPage;
