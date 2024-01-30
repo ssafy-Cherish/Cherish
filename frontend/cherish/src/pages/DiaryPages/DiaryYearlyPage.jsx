2d491496d20c9c1fe4b548a6301f9c31b573546b
전찬혁 <cksgur5530@gmail.com>
2024-01-30 22:57:56 +0900

    Fix : 이미지 형식으로 변경
    이슈 코드 : [S10P12D103-158]
    
    - assets/Month에 월마다 사용할  svg 파일 추가
      - 하나의 공통 이미지를 통해 구현하려고 했는데 실패했습니다.
      - 그래서 이미지를 여러 개 사용하는 것으로 전환했씁니다.
      - 장점 : 코드 짧아짐, 이전 커밋에서 화면 비율 조정시 망가지는 문제
        부분 해결
      - 단점 : import를 많이 함
    - 애니메이션 추가
      - 연도 이동 시 월간 이미지가 사라졌다가 나타나게 변경
    - 이동 방식 변경
      - 기존의 Link에서 navigate로 변경

--- frontend/cherish/src/pages/DiaryPages/DiaryYearlyPage.jsx
+++ frontend/cherish/src/pages/DiaryPages/DiaryYearlyPage.jsx
@@ -1,64 +1,81 @@
 import Modal from "../../components/Diary/Modal";
-import yearlyImg from "../../assets/DiaryYearlyPage.svg";
-import { Link, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { motion, useAnimate } from "framer-motion";
 
+import Month1 from "../../assets/Month/Month1.svg";
+import Month2 from "../../assets/Month/Month2.svg";
+import Month3 from "../../assets/Month/Month3.svg";
+import Month4 from "../../assets/Month/Month4.svg";
+import Month5 from "../../assets/Month/Month5.svg";
+import Month6 from "../../assets/Month/Month6.svg";
+import Month7 from "../../assets/Month/Month7.svg";
+import Month8 from "../../assets/Month/Month8.svg";
+import Month9 from "../../assets/Month/Month9.svg";
+import Month10 from "../../assets/Month/Month10.svg";
+import Month11 from "../../assets/Month/Month11.svg";
+import Month12 from "../../assets/Month/Month12.svg";
+
+const MonthArr = [
+	Month1,
+	Month2,
+	Month3,
+	Month4,
+	Month5,
+	Month6,
+	Month7,
+	Month8,
+	Month9,
+	Month10,
+	Month11,
+	Month12,
+];
+
 const DiaryYearlyPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [scope, animate] = useAnimate();
-  let year = searchParams.get("year");
-  if (!year) year = new Date().getFullYear();
+	const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [scope, animate] = useAnimate();
+	let year = searchParams.get("year");
+	if (!year) year = new Date().getFullYear();
 
-  const nums = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-  const numColor = [
-    "shadow-cyan-500/50",
-    "shadow-orange-500/50",
-    "shadow-blue-500/50",
-    "shadow-red-500/50",
-  ];
+	function moveYear(move) {
+		animate("#year", { y: [0, -5, 5, 0] }, { duration: 0.2 });
+		animate("#month", { opacity: [1, 0, 1] });
+		year = +year + move;
+		searchParams.set("year", year);
+		setSearchParams(searchParams);
+	}
 
-  function moveYear(move) {
-    animate("#year", { y: [0, -5, 5, 0] }, { duration: 0.2 });
-    year = +year + move;
-    searchParams.set("year", year);
-    setSearchParams(searchParams);
-  }
+	function moveToMonth(month) {
+		navigate({ pathname: "/diary/month", search: `?year=${year}&month=${month}` });
+	}
 
-  return (
-    <Modal img={yearlyImg} z={1} height="90vh" width="auto">
-      <div style={{ margin: "1rem 30%" }}>
-        <div className="text-[3vw] text-red-400 text-center" ref={scope}>
-          <button onClick={() => moveYear(-1)}>&lt;</button>
-          <div id="year" className="inline-block text-[2vw]">
-            &nbsp;{year}&nbsp;
-          </div>
-          <button onClick={() => moveYear(1)}>&gt;</button>
-        </div>
-        <div className="grid grid-cols-3 gap-x-3 gap-y-6">
-          {nums.map((num) => (
-            <Link key={num} to={{ pathname: `/diary/month`, search: `?year=${year}&month=${num}` }}>
-              <motion.button whileHover={{ scale: 1.2 }}>
-                <div className="w-[186px] h-44 left-0 top-0 relative ">
-                  <div
-                    className={`w-[186px] h-44 left-0 top-0 absolute bg-white rounded-[30px] border border-neutral-400 shadow-md ${
-                      numColor[parseInt((Number(num) % 12) / 3)]
-                    }`}
-                  />
-                  <div className="left-[32px] top-[25px] absolute text-center text-black text-[48px] font-light font-['Gluten']">
-                    {num}
-                  </div>
-                  <div className="w-[49px] h-[0px] left-[93px] top-[66px] absolute border border-neutral-400"></div>
-                  <div className="w-[99.02px] h-[0px] left-[43px] top-[94px] absolute border border-neutral-400"></div>
-                  <div className="w-[75px] h-[0px] left-[43px] top-[139px] absolute border border-neutral-400"></div>
-                  <div className="w-[99.02px] h-[0px] left-[43px] top-[118px] absolute border border-neutral-400"></div>
-                </div>
-              </motion.button>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal z={1} height="90vh" width="70vw">
+			{/* <img src={yearlyImg} alt="" className="h-full w-full absolute -z-10" /> */}
+			<div className="h-full w-full grid grid-rows-12 px-[10%] py-10" ref={scope}>
+				<div className="text-[2vw] text-red-400 text-center row-span-1">
+					<button onClick={() => moveYear(-1)}>&lt;</button>
+					<div id="year" className="inline-block text-[2vw]">
+						&nbsp;{year}&nbsp;
+					</div>
+					<button onClick={() => moveYear(1)}>&gt;</button>
+				</div>
+				<div className="grid grid-cols-3 gap-x-6 gap-y-6 row-span-11 px-[10%]">
+					{MonthArr.map((src, i) => (
+						<motion.img
+							key={src}
+							id="month"
+							src={src}
+							alt={src}
+							whileHover={{ scale: 1.2 }}
+							className="mx-auto"
+							onClick={() => moveToMonth(i + 1)}
+						/>
+					))}
+				</div>
+			</div>
+		</Modal>
+	);
 };
 
 export default DiaryYearlyPage;
