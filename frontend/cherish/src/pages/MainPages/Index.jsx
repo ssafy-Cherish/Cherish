de800fc86fe86012d11abf2e412949df65aa3efa
김경범 <gung2227@naver.com>
2024-01-31 10:09:22 +0900

    Feat : 메인페이지 화면 구현 (기능은 아직 추가안함)
    이슈 코드 : [S10P12D103-175]
    
    - 캐로셸 여백추가
    - 메모 및 메인페이지 화면 추가

--- frontend/cherish/src/pages/MainPages/Index.jsx
+++ frontend/cherish/src/pages/MainPages/Index.jsx
@@ -11,7 +11,7 @@ import MemoImg from "../../assets/MemoImg.svg"
 export default function Index() {
   
   return (
-    <div className="mt-[5.6rem]">
+    <div className="mt-[4vw]">
       <div className="flex justify-between">
         <p className="text-[2.5vw]">
           우리 만난지 <span className="text-cherry">99</span>일 째!
@@ -22,7 +22,7 @@ export default function Index() {
             <p className="text-[1vw]">
               오늘의 질문이 들어갈 예정솰로살라솰라이에요!
             </p>
-            <div className="bg-white w-[1.5vw] h-[1.5vw] rounded-full shadow-inner"></div>
+            <div className="bg-white w-[1.5vw] h-[1.5vw] rounded-full drop-shadow-inner"></div>
           </div>
           <div className="absolute z-10 ml-[34vw] mb-[5px]">
             <img src={MainBookmarkIcon} alt="" />
@@ -30,42 +30,44 @@ export default function Index() {
         </div>
       </div>
       <Carousel />
-      <div className="LinkCradWrapper mt-5 h-[17vw] grid grid-cols-9 gap-5">
-        <div className="Pot bg-pink col-span-3 grid grid-cols-5">
-          <div className="PotState col-span-2 flex flex-col items-center">
-            <div className="PotImg">
+      <div id='LinkCradWrapper' className="mt-5 h-[17vw] grid grid-cols-9 gap-5">
+        <div id='Pot' className="bg-pink col-span-3 grid grid-cols-5 rounded-[20px] shadow-md">
+          <div id='PotState' className="col-span-2 flex flex-col items-center">
+            <div id='PotImg'>
               <img src={PotLv1} alt="PotImg" />
             </div>
-            <p className="text-[1.2vw] text-cherry">체리 떡잎</p>
+            <p id='PotName'className="text-[1.2vw] text-cherry">체리 떡잎</p>
             <progress id="PotExp" className="PotExp h-2 w-[7vw] mt-5" max="100" value="70" />
           </div>
-          <div className="PotDescrption col-span-3 flex flex-col items-center justify-around">
-            <p className="text-[1.5vw]">알콩달콩 <span className="text-cherry">커플</span></p>
-            <div className="flex flex-row items-center">
+          <div id='PotDescrption' className="col-span-3 flex flex-col items-center justify-around">
+            <p id='PotCouple' className="text-[1.5vw]">알콩달콩 <span className="text-cherry">커플</span></p>
+            <div id='PotCoupleName' className="flex flex-row items-center">
               <p className="text-[1.2vw]">나희도</p>
-              <div>
-                <img src={HeartIcon} alt="HeartIcon" />
-              </div>
+              <div><img src={HeartIcon} alt="HeartIcon" /></div>
               <p className="text-[1.2vw]">백이진</p>
             </div>
             <p className="text-[1.2vw]">D + 99</p>
-            <button className="text-[1.2vw]">경험치 기록 보기</button>
+            <button id='GoPot' className="text-[1.2vw]">경험치 기록 보기</button>
           </div>
         </div>
-        <div className="Diary bg-skyblue col-span-2 flex flex-col items-center">
-          <div className="mt-[1vw]"><img src={DiaryImg} alt="" /></div>
+        <div id='Diary' className="bg-skyblue col-span-2  flex flex-col items-center rounded-[20px] shadow-md">
+          <div className="mt-[1vw]"><img src={DiaryImg} alt="DiaryImg" /></div>
           <p className="mt-[1vw] text-[1.2vw]">다이어리</p>
           </div>
-        <div className="CherryCall bg-beige col-span-2 flex flex-col items-center justify-around">
-          <p className="text-[1.2vw]"><span className="text-cherry">체리콜</span>로 화분을 키워요!</p>
-          <div><img src={CoupleImg1} alt="" /></div>
-          <p className="text-[1.2vw]">체리콜</p></div>
-        <div className="Memo col-span-2 relative">
-          <div className=" w-[17vw]">
-            <img src={MemoImg} alt="MemoImg" />
+        <div id='CherryCall' className="bg-beige col-span-2 flex flex-col items-center justify-around rounded-[20px] shadow-md">
+          <p className="text-[1.2vw] mt-[1vw]"><span className="text-cherry">체리콜</span>로 화분을 키워요!</p>
+          <div><img src={CoupleImg1} alt="CoupleImg1" /></div>
+          <p className="text-[1.2vw] mb-[1.8vw]">체리콜</p></div>
+        <div id='Memo' className="col-span-2 relative">
+          <div className="w-[17vw]">
+          <div className="absolute mt-[4.5vw] ml-[4.5vw]">
+            <p className="text-[1vw] text-center">
+              <span>Memo</span>
+            <br /><br /> 세줄세줄세줄세줄
+            <br />세줄세줄세줄세줄
+            <br />세줄세줄세줄세줄</p>
           </div>
-          <div className="absolute">
-          <p className="mt-[5vw] ml-[2vw]">Memo</p>
+            <img className="w-full" src={MemoImg} alt="MemoImg" />
           </div>
           </div>
       </div>
