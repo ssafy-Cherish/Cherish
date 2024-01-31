5768d8b54eec17363386e79a8ba251397da89edf
김준엽 <wnsduq1025@gmail.com>
2024-01-31 22:00:23 +0900

    Feat : memo Mapper, Service 추가
    이슈 코드 : [S10P12D103]

---
+++ backend/cherish/src/main/java/com/ssafy/cherish/memo/model/service/MemoService.java
@@ -0,0 +1,14 @@
+package com.ssafy.cherish.memo.model.service;
+
+import com.ssafy.cherish.memo.model.dto.MemoDto;
+
+import java.sql.SQLException;
+
+public interface MemoService {
+
+    void writeMemo (MemoDto memoDto) throws Exception;
+    MemoDto getMemo (String date, String coupleId) throws Exception;
+    void modifyMemo (MemoDto memoDto) throws Exception;
+    void deleteMemo (String date, String coupleId) throws Exception;
+
+}
