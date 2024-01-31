4304d48327dcecdbe8c80b5942a4e6f47efdb47f
김예현 <qkdo12@naver.com>
2024-01-31 16:32:10 +0900

    Feat : FFmpeg 활용하여 임의의 동영상 연속되도록 병합 기능 구현
    이슈 코드 : [S10P12D103-214]
    
    - FFmpeg Wrapper (자바와 로컬의 ffmpeg 연결해주는 라이브러리) 추가
    - cron (특정시간마다 자동으로 작업수행해주는 기능) 추가
        - 의도하지 않은 시간에 실행되지 않도록 주석 처리

--- backend/cherish/src/main/java/com/ssafy/cherish/utils/CronScheduler.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/utils/CronScheduler.java
@@ -5,7 +5,8 @@ import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,36 +21,43 @@ public class CronScheduler {
 
     //매월 1일로 바뀔때 실행됨
     //@Scheduled(cron = "0 0 0 1 * *")
-    //아래(테스트용)는 10분마다 실행됨
-    @Scheduled(cron = "0/10 * * * * *")
+    //아래는 테스트용 (주석 빼면 10초에 한번씩 실행됨)
+//    @Scheduled(cron = "0/10 * * * * *")
     public void mergeMonthlyVideo()
     {
         System.out.println("테스트");
         try {
             //TODO: AWS 환경에 맞게 변경해야함
             //window 기준, 로컬에 설치해야함
-            //String ffmpegPath="C:\\Program Files\\ffmpeg-6.1.1-essentials_build\\bin";
+            String ffmpegPath="C:\\Program Files\\ffmpeg-6.1.1-essentials_build\\bin\\ffmpeg.exe";
+            String ffprobePath="C:\\Program Files\\ffmpeg-6.1.1-essentials_build\\bin\\ffprobe.exe";
             //mac 기준
-            String ffmpegPath=System.getProperty("user.home")+"/ffmpeg/6.1.1_3/bin";
+            //String ffmpegPath=System.getProperty("user.home")+"/ffmpeg/6.1.1_3/bin";
 
             FFmpeg ffmpeg = new FFmpeg(ffmpegPath);
-            FFprobe ffprobe = new FFprobe(ffmpegPath);
+            FFprobe ffprobe = new FFprobe(ffprobePath);
 
-            String filepath=System.getProperty("user.home")+"/Documents/cherish_video/1/1/";
+            FFmpegProbeResult videoProbe1=ffprobe.probe("C:/Users/SSAFY/Documents/cherish_video/1/1/1.mp4");
 
-            //ffmpeg -f concat -safe 0 -i `text file input path` `out path`
-            //ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.wav
+
+//            filepath 경로에 있는 list.txt에
+//            / file 'C:\Users\SSAFY\Documents\cherish_video\1\1\1.mp4' /
+//            형식의 텍스트 저장 필요
+
+            String filepath="C:/Users/SSAFY/Documents/cherish_video/1/1/";
+            //ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.mp4
             FFmpegBuilder builder=new FFmpegBuilder()
                     .overrideOutputFiles(true)
-                    .addInput(filepath+"1.mp4")
-                    .addInput(filepath+"2.mp4")
+                    .addInput("C:/Users/SSAFY/list.txt")
                     .addExtraArgs("-f","concat")
                     .addExtraArgs("-safe","0")
                     .addOutput(filepath+"output.mp4")
                     .done();
 
+            log.debug("FFmpeg command: {}", builder.build());
+
             FFmpegExecutor executor = new FFmpegExecutor(ffmpeg,ffprobe);
-            executor.createTwoPassJob(builder).run();
+            executor.createJob(builder).run();
 
         }catch (IOException e)
         {
