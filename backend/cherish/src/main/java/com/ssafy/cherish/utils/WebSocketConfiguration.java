57ff56b4ba7395788d683db16038b7f947133bea
이승준 <tmdwns7800@naver.com>
2024-01-31 17:49:24 +0900

    Feat : WebRTC 양방향 통신 기능 추가중
    이슈 코드 : [S10P12D103-189]

--- backend/cherish/src/main/java/com/ssafy/cherish/utils/WebSocketConfiguration.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/utils/WebSocketConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
 
+    // 웹 소켓 핸들러를 등록한뒤 CORS를 처리
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new SocketHandler(), "/socket")
