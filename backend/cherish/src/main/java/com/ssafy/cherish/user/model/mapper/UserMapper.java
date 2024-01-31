237050defa8c56626fce68919d6c04761158f416
김준엽 <wnsduq1025@gmail.com>
2024-01-31 14:01:15 +0900

    Fix : 회원가입 로직 수정
    이슈 코드 : [S10P12D103]

--- backend/cherish/src/main/java/com/ssafy/cherish/user/model/mapper/UserMapper.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/user/model/mapper/UserMapper.java
@@ -10,23 +10,27 @@ import java.sql.SQLException;
 @Mapper
 public interface UserMapper {
 
-    // 그냥 회원가입으로 변경
+    // 원래 유저를 나눠서 회원가입을 진행하려고 했지만 그냥 회원가입으로 변경
+    // 먼저 insertKakaoId를 통해 user 테이블을 생성 후 이걸로 유저 회원가입을 할 것이다ㅏㅏㅏ 알겠는가
     void join (UserDto userDto) throws SQLException;
 
-    // 첫 번째 유저 회원가입
-    void firstJoin (UserDto userDto) throws SQLException;
 
-    // 두 번째 유저 회원가입
-    void secondJoin (UserDto userDto) throws SQLException;
-
-    // 첫 번째 유저 회원가입 시 커플 테이블 생성
+    // 첫 번째 유저 회원가입 시 커플 테이블 업데이트
     void coupleFirstJoin (CoupleDto coupleDto) throws SQLException;
 
     // 두 번째 유저 회원가입 시 커플 테이블 업데이트
     void coupleSecondJoin (CoupleDto coupleDto) throws SQLException;
 
-    //회원 정보 조회
-    UserDto userInfo(int kakaoId) throws SQLException;
+    // 카카오 아이디를 받아 user 테이블 생성 -> 회원가입의 시작이라고 할 수 있죠 후후,,,
+    void insertKakaoId (long kakaoId) throws SQLException;
 
+    //회원 정보 조회
+    UserDto userInfo (int id) throws SQLException;
+
+    // 커플 정보 조회
+    CoupleDto coupleInfo (int id) throws SQLException;
+
+    // 카카오 아이디 찾기 -> 처음에 접속했을 때 유저 정보에 있는지 없는지 확인하는 것입니당 !! 알겠읍니까 휴먼?
+    UserDto findByKakaoId (long kakaoId) throws SQLException;
 
 }
