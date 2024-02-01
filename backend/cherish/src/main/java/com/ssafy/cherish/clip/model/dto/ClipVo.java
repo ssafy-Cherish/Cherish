8ec989370426a39c3489f967b172539c669110a2
김예현 <qkdo12@naver.com>
2024-02-01 17:52:15 +0900

    Feat: saveMonthlyVideo와 mergeMonthlyVideo 구분
    이슈 코드 : [S10P12D103-214]
    
    - 월별, 키워드별, 커플별로 누적클립수가 5개 이상인 클립들을 ClipVo에
      저장하여 반환하는 mybatis 쿼리 메서드 추가

---
+++ backend/cherish/src/main/java/com/ssafy/cherish/clip/model/dto/ClipVo.java
@@ -0,0 +1,13 @@
+package com.ssafy.cherish.clip.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ClipVo {
+    private final int id;
+    private final String keyword;
+    private final int coupleId;
+    private final String filepath;
+}
