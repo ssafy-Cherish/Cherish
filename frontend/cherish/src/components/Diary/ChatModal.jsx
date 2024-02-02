4bd7156c3bf55e2789e0408d38e11a55b4960dc9
전찬혁 <cksgur5530@gmail.com>
2024-02-02 21:19:23 +0900

    Feat : Memo 수정 기능 추가
    이슈 코드 : [S10P12D103-159]
    
    - MemoModal 구현
      - 수정 기능
      - 저장 및 취소 기능

--- frontend/cherish/src/components/Diary/ChatModal.jsx
+++ frontend/cherish/src/components/Diary/ChatModal.jsx
@@ -3,47 +3,53 @@ import "../../components/Diary/DiaryDailyPage.css";
 import Modal from "./Modal";
 
 const ChatModal = ({ chats, onClose, myId }) => {
-  return (
-    <Modal onClose={onClose} dialogCss="w-[30vw] h-[60vh] bg-pink p-5 rounded-2xl">
-      <div className="relative rounded-b-2xl h-full">
-        <div className="bg-white rounded-2xl h-full overflow-y-auto absolute w-full p-5 rmscroll">
-          {chats.map((chat, idx) => {
-            return (
-              <>
-                {myId === chat.kakaoId ? (
-                  <div key={idx} className="flex flex-row justify-end pl-8 pr-4 pt-4 ">
-                    <div
-                      style={{
-                        backgroundColor: "#FEF8EC",
-                        whiteSpace: "pre-line",
-                        wordWrap: "break-word",
-                      }}
-                      className="py-2 pl-4 pr-4 rounded-tl-xl rounded-b-xl drop-shadow max-w-[80%]"
-                    >
-                      {chat.content}
-                    </div>
-                  </div>
-                ) : (
-                  <div key={idx} className="flex flex-row justify-start pl-4 pr-8 pt-4">
-                    <div
-                      style={{
-                        backgroundColor: "#E0F4FF",
-                        whiteSpace: "pre-line",
-                        wordWrap: "break-word",
-                      }}
-                      className="py-2 pl-4 pr-4 rounded-tr-xl rounded-b-xl drop-shadow max-w-[80%]"
-                    >
-                      {chat.content}
-                    </div>
-                  </div>
-                )}
-              </>
-            );
-          })}
-        </div>
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal onClose={onClose} dialogCss="w-[30vw] h-[60vh] bg-pink p-5 rounded-2xl" isX={true}>
+			<div className="relative rounded-b-2xl h-full">
+				<div className="bg-white rounded-2xl h-full overflow-y-auto absolute w-full p-5 rmscroll">
+					{chats.map((chat, idx) => {
+						return (
+							<>
+								{myId === chat.kakaoId ? (
+									<div
+										key={idx}
+										className="flex flex-row justify-end pl-8 pr-4 pt-4 "
+									>
+										<div
+											style={{
+												backgroundColor: "#FEF8EC",
+												whiteSpace: "pre-line",
+												wordWrap: "break-word",
+											}}
+											className="py-2 pl-4 pr-4 rounded-tl-xl rounded-b-xl drop-shadow max-w-[80%]"
+										>
+											{chat.content}
+										</div>
+									</div>
+								) : (
+									<div
+										key={idx}
+										className="flex flex-row justify-start pl-4 pr-8 pt-4"
+									>
+										<div
+											style={{
+												backgroundColor: "#E0F4FF",
+												whiteSpace: "pre-line",
+												wordWrap: "break-word",
+											}}
+											className="py-2 pl-4 pr-4 rounded-tr-xl rounded-b-xl drop-shadow max-w-[80%]"
+										>
+											{chat.content}
+										</div>
+									</div>
+								)}
+							</>
+						);
+					})}
+				</div>
+			</div>
+		</Modal>
+	);
 };
 
 export default ChatModal;
--- frontend/cherish/src/components/Diary/MemoModal.jsx
+++ frontend/cherish/src/components/Diary/MemoModal.jsx
@@ -1,13 +1,86 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "../../components/Diary/DiaryDailyPage.css";
 import Modal from "./Modal";
 
-const ChatModal = ({ content, memoId, onClose }) => {
-  return (
-    <Modal onClose={onClose} dialogCss="w-[30vw] h-[60vh] bg-pink p-5 rounded-2xl">
-      <div></div>
-    </Modal>
-  );
+const ChatModal = ({ onClose, memo, setMemo }) => {
+	const [isWriting, setIsWriting] = useState();
+	const [content, setContent] = useState(memo.content);
+
+	function changeContent(event) {
+		setContent(event.target.value);
+	}
+
+	function cancelContent() {
+		setContent(memo.content);
+		setIsWriting(false);
+	}
+
+	function saveContent() {
+		// TODO : 업데이트 쿼리 날려야 함, 또는 memoId가 없다면 insert
+
+		setMemo((prev) => ({ ...prev, content: content }));
+		setIsWriting(false);
+	}
+
+	return (
+		<Modal
+			onClose={onClose}
+			dialogCss="w-[30vw] h-[60vh] bg-skyblue p-5 rounded-2xl relative"
+			isX={true}
+		>
+			<div className="flex flex-col h-full">
+				<div className="flex flex-row justify-center max-w-full ">
+					<div className="text-[2vw] italic">Memo</div>
+				</div>
+				<div className="divider  divider-start italic">Content</div>
+				<div className="h-full max-h-full p-[1vw]">
+					{isWriting ? (
+						<textarea
+							cols="30"
+							rows="10"
+							className="w-full h-full resize-none rounded-xl drop-shadow-xl"
+							value={content}
+							onChange={changeContent}
+						></textarea>
+					) : (
+						<div
+							style={{
+								backgroundColor: "#E0F4FF",
+								whiteSpace: "pre-line",
+								wordWrap: "break-word",
+							}}
+							className="rounded-xl drop-shadow-xl h-full "
+						>
+							{memo.content}
+						</div>
+					)}
+				</div>
+				<div className="flex flex-row justify-around">
+					{isWriting ? (
+						<>
+							<div
+								onClick={cancelContent}
+								className="btn btn-outline bg-white text-cherry"
+							>
+								취소
+							</div>
+							<div onClick={saveContent} className="btn bg-cherry text-white">
+								저장
+							</div>
+						</>
+					) : (
+						<div
+							onClick={() => setIsWriting(true)}
+							className="btn btn-outline bg-white text-cherry"
+						>
+							수정
+						</div>
+					)}
+				</div>
+			</div>
+		</Modal>
+	);
 };
 
 export default ChatModal;
