7f3a0e62ee9b97132eb6a39b4783266630c67d05
김경범 <gung2227@naver.com>
2024-02-02 17:52:30 +0900

    오늘의 질문 :: 미완성

--- frontend/cherish/src/pages/TodayQuestionPages/TodayQuestionRecodePage.jsx
+++ frontend/cherish/src/pages/TodayQuestionPages/TodayQuestionRecodePage.jsx
@@ -1,11 +1,31 @@
-import Modaltest from "../../components/Common/ModalTest";
+import dayjs from "dayjs";
+import TodayRecoding from "../../components/TodayQuestion/TodayRecoding";
+import ModalRoute from "../../components/Common/ModalRoute";
+import NextIcon from "../../assets/NextIcon.svg";
+
 export default function TodayQuestionRecodePage() {
-  const date = new Date()
-  console.log(date)
+  const date = dayjs();
+  const year = date.get("y");
+  const month = date.get("M") + 1;
+  const day = date.get("D");
 
   return (
-    <Modaltest modalcss="w-[49.5vw] h-[85vh] bg-pink rounded-[40px]" isX={true}>
-      test
-    </Modaltest>
+    <ModalRoute
+      modalcss="w-[49.5vw] h-[85vh] bg-pink rounded-[40px]"
+      isX={true}
+    >
+      <div className="flex flex-col items-center justify-center mt-[2vw] ml-[2.5vw]">
+        <p className="text-[1.8vw]">
+          {year}년 {month}월 {day}일
+        </p>
+        <button className="bg-white h-[3vw] w-[42vw] mt-[1vw] rounded-[35px] text-[1.3vw] shadow-md text-center leading-[3vw]">
+          오늘의 질문란
+          <div className="float-right">
+            <img src={NextIcon} alt="" />
+          </div>
+        </button>
+        <TodayRecoding />
+      </div>
+    </ModalRoute>
   );
 }
