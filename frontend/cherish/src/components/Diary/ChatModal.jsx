715ae1386f671404eab320bf827c73393531e977
전찬혁 <cksgur5530@gmail.com>
2024-02-02 16:14:42 +0900

    Feat : Diary Daily, Clip 별 pin 설정 및 키워드 추가
    이슈 코드 : [S10P12D103-157]
    
    - DiaryDailyPage.jsx
      - pin svg 추가, 클릭시 색 토글
      - 클립에 마우스 올릴 시 keyword가 왼쪽에 나오게 설정
      - 채팅 버튼 크기 유동적으로 바뀌게 변경
      - 타임라인 그리드 재 설정

--- frontend/cherish/src/components/Diary/ChatModal.jsx
+++ frontend/cherish/src/components/Diary/ChatModal.jsx
@@ -1,92 +1,80 @@
 import { createPortal } from "react-dom";
 import { motion, useAnimate } from "framer-motion";
-import CloseIcon from "../../assets/CloseIcon.svg";
+import "../../components/Diary/DiaryDailyPage.css";
 
-const ChatModal = ({ z, chats, onClose, isX, myId }) => {
-	const [scope, animate] = useAnimate();
-	console.log(chats);
+const ChatModal = ({ z, chats, onClose, myId }) => {
+  const [scope, animate] = useAnimate();
 
-	const backdropStyle = {
-		position: "fixed",
-		top: "0",
-		left: "0",
-		width: "100%",
-		height: "100vh",
-		backgroundColor: "rgba(0, 0, 0, 0.25)",
-		zIndex: z || "9",
-	};
-	const modalStyle = {
-		margin: "10% auto",
-		maxWidth: "90%",
-		zIndex: "10",
-	};
+  const backdropStyle = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
+    zIndex: z || "9",
+  };
+  const modalStyle = {
+    margin: "10% auto",
+    maxWidth: "90%",
+    zIndex: "10",
+  };
 
-	return createPortal(
-		<div onClick={onClose} ref={scope} style={backdropStyle}>
-			<motion.dialog
-				className="w-[30vw] h-[60vh] bg-pink p-5 rounded-2xl"
-				initial={{ opacity: 0, y: 30 }}
-				animate={{ opacity: 1, y: 0 }}
-				exit={{ opacity: 0, y: 30 }}
-				open
-				onClick={(e) => {
-					// 다이얼로그 안을 클릭 했을 때 onClose가 실행되는 이벤트 캡쳐링 방지
-					e.stopPropagation();
-				}}
-				style={modalStyle}
-			>
-				{isX && (
-					<button onClick={onClose} className="float-right mt-[1.5vw] mr-[1.5vw]">
-						<img src={CloseIcon} alt="CloseIcon" />
-					</button>
-				)}
-				<div className="relative rounded-b-2xl h-full">
-					<div className="bg-white rounded-2xl h-full overflow-y-auto absolute w-full p-5">
-						{chats.map((chat, idx) => {
-							return (
-								<>
-									{myId === chat.kakaoId ? (
-										<div
-											key={idx}
-											className="flex flex-row justify-end pl-8 pr-4 pt-4 "
-										>
-											<div
-												style={{
-													backgroundColor: "#FEF8EC",
-													whiteSpace: "pre-line",
-													wordWrap: "break-word",
-												}}
-												className="py-2 pl-4 pr-4 rounded-tl-xl rounded-b-xl drop-shadow"
-											>
-												{chat.content}
-											</div>
-										</div>
-									) : (
-										<div
-											key={idx}
-											className="flex flex-row justify-start pl-4 pr-8 pt-4"
-										>
-											<div
-												style={{
-													backgroundColor: "#E0F4FF",
-													whiteSpace: "pre-line",
-													wordWrap: "break-word",
-												}}
-												className="py-2 pl-4 pr-4 rounded-tr-xl rounded-b-xl drop-shadow max-w-full"
-											>
-												{chat.content}
-											</div>
-										</div>
-									)}
-								</>
-							);
-						})}
-					</div>
-				</div>
-			</motion.dialog>
-		</div>,
-		document.body
-	);
+  return createPortal(
+    <div onClick={onClose} ref={scope} style={backdropStyle}>
+      <motion.dialog
+        className="w-[30vw] h-[60vh] bg-pink p-5 rounded-2xl"
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }}
+        open
+        onClick={(e) => {
+          // 다이얼로그 안을 클릭 했을 때 onClose가 실행되는 이벤트 캡쳐링 방지
+          e.stopPropagation();
+        }}
+        style={modalStyle}
+      >
+        <div className="relative rounded-b-2xl h-full">
+          <div className="bg-white rounded-2xl h-full overflow-y-auto absolute w-full p-5 rmscroll">
+            {chats.map((chat, idx) => {
+              return (
+                <>
+                  {myId === chat.kakaoId ? (
+                    <div key={idx} className="flex flex-row justify-end pl-8 pr-4 pt-4 ">
+                      <div
+                        style={{
+                          backgroundColor: "#FEF8EC",
+                          whiteSpace: "pre-line",
+                          wordWrap: "break-word",
+                        }}
+                        className="py-2 pl-4 pr-4 rounded-tl-xl rounded-b-xl drop-shadow max-w-[80%]"
+                      >
+                        {chat.content}
+                      </div>
+                    </div>
+                  ) : (
+                    <div key={idx} className="flex flex-row justify-start pl-4 pr-8 pt-4">
+                      <div
+                        style={{
+                          backgroundColor: "#E0F4FF",
+                          whiteSpace: "pre-line",
+                          wordWrap: "break-word",
+                        }}
+                        className="py-2 pl-4 pr-4 rounded-tr-xl rounded-b-xl drop-shadow max-w-[80%]"
+                      >
+                        {chat.content}
+                      </div>
+                    </div>
+                  )}
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </motion.dialog>
+    </div>,
+    document.body
+  );
 };
 
 export default ChatModal;
