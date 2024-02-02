6f3f989488e4ccc8aa7cf3a15dbddb8776cba0ad
전찬혁 <cksgur5530@gmail.com>
2024-02-02 17:42:59 +0900

    Feat : MemoModal 추가
    이슈 코드 : [S10P12D103-159]
    
    - openMemoModal state 추가

--- frontend/cherish/src/components/Diary/Modal.jsx
+++ frontend/cherish/src/components/Diary/Modal.jsx
@@ -1,46 +1,41 @@
-import { motion, useAnimate } from "framer-motion";
-import { useNavigate } from "react-router-dom";
 import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
-import classes from "./Modal.module.css";
+const Modal = ({ z, onClose, children, dialogCss }) => {
+  const backdropStyle = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
+    zIndex: z || "9",
+  };
+  const modalStyle = {
+    margin: "10% auto",
+    maxWidth: "90%",
+    zIndex: "10",
+  };
 
-const Modal = ({ children, z, width, height }) => {
-	const navigate = useNavigate();
-	const [scope, animate] = useAnimate();
-	function onClose() {
-		animate("dialog", { opacity: 0, y: 30 });
-		setTimeout(() => {
-			navigate("..");
-		}, 300);
-	}
-
-	return createPortal(
-		<div
-			className={classes.backdrop}
-			onClick={onClose}
-			ref={scope}
-			style={{ zIndex: z ? z : undefined }}
-		>
-			<motion.dialog
-				className={classes.modal}
-				initial={{ opacity: 0, y: 30 }}
-				animate={{ opacity: 1, y: 0 }}
-				open
-				onClick={(e) => {
-					// 다이얼로그 안을 클릭 했을 때 onClose가 실행되는 이벤트 캡쳐링 방지
-					e.stopPropagation();
-				}}
-				style={{
-					height: height ? height : undefined,
-					width: width ? width : undefined,
-					padding: 0,
-				}}
-			>
-				{children}
-			</motion.dialog>
-		</div>,
-		document.body
-	);
+  return createPortal(
+    <div onClick={onClose} style={backdropStyle}>
+      <motion.dialog
+        className={dialogCss}
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }}
+        open
+        onClick={(e) => {
+          // 다이얼로그 안을 클릭 했을 때 onClose가 실행되는 이벤트 캡쳐링 방지
+          e.stopPropagation();
+        }}
+        style={modalStyle}
+      >
+        {children}
+      </motion.dialog>
+    </div>,
+    document.body
+  );
 };
 
 export default Modal;
