c70e32843246c0172cd3be9dc050601495257d82
전찬혁 <cksgur5530@gmail.com>
2024-02-04 18:41:02 +0900

    Feat : Kakao API 구현
    이슈 코드 : [S10P12D103-163]
    
    - KAKAO API 설정 및 구현
      - 로그인 API
      - 로그아웃 API
      - 메시지 공유 API
      - 서비스 연결 끊기 API
    - 커플 대기화면 디자인

--- frontend/cherish/src/components/User/SocialKakao.jsx
+++ frontend/cherish/src/components/User/SocialKakao.jsx
@@ -1,23 +1,85 @@
-import KakaoLogin from "react-kakao-login";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
+import Kakao from "../../utils/Kakao";
+import WaitingModal from "./WaitingModal";
+import { AnimatePresence } from "framer-motion";
+import KakaoLogin from "../../assets/User/kakao_login_large_wide.png";
+
 const SocialKakao = () => {
-	const kakaoClientId = import.meta.env.VITE_APP_KAKAO_JAVASCRIPT_KEY;
 	const navigate = useNavigate();
+	const [openWaitingModal, setOpenWaitingModal] = useState(false);
 
-	const kakaoOnSuccess = async (data) => {
-		console.log(data);
-		const idToken = data.response.access_token; // 엑세스 토큰 백엔드로 전달
+	const [coupleCode, setCoupleCode] = useState("!VCJmYMh3LP*9@j");
 
-		// 이미 있는 회원인지 확인해서 메인으로 보내거나 회원가입을 보내거나 선택
-		navigate("/user/signup");
-	};
-	const kakaoOnFailure = (error) => {
-		console.log(error);
-	};
+	function handleKakaoLogin() {
+		// TODO: 여러 번 클릭누르는거 막아둬야 함
+
+		Kakao.Auth.login({
+			success(data) {
+				console.log(data);
+
+				Kakao.API.request({
+					url: "/v2/user/me",
+					success(info) {
+						console.log(info);
+
+						// 회원이 없으면
+						// navigate("/user/signup");
+
+						// 회원이 있는데 coupled false면
+						// TODO: setCoupleCode
+						setOpenWaitingModal(true);
+
+						// 회원이 있는데 coupled true면
+						// navigate("/");
+					},
+					fail(err) {
+						console.log(err);
+					},
+				});
+			},
+			fail(err) {
+				console.log(err);
+			},
+		});
+	}
+
+	function handleKakaoLogout() {
+		if (!Kakao.Auth.getAccessToken()) {
+			alert("로그인중이 아닙니다.");
+			return;
+		}
+		Kakao.Auth.logout(() => {
+			alert("로그아웃");
+		});
+	}
+
+	function unlinkUser() {
+		if (!Kakao.Auth.getAccessToken()) {
+			alert("로그인중이 아닙니다.");
+			return;
+		}
+
+		Kakao.API.request({
+			url: "/v1/user/unlink",
+			success(res) {
+				console.log(res);
+			},
+			fail(err) {
+				console.log(err);
+			},
+		});
+	}
+
 	return (
 		<>
-			<KakaoLogin token={kakaoClientId} onSuccess={kakaoOnSuccess} onFail={kakaoOnFailure} />
+			<img src={KakaoLogin} onClick={handleKakaoLogin} className="w-[80%]" />
+			<AnimatePresence>
+				{openWaitingModal && (
+					<WaitingModal onClose={() => setOpenWaitingModal(false)} code={coupleCode} />
+				)}
+			</AnimatePresence>
 		</>
 	);
 };
