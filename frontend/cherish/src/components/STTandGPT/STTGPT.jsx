78ee4440afdb56d70eb45398bacf5cfe90c7d9af
이승준 <tmdwns7809@gmail.com>
2024-02-04 19:36:21 +0900

    Fix : CORS 허용, api 서버 요청 확인
    이슈 코드 : [S10P12D103-219]

--- frontend/cherish/src/components/STTandGPT/STTGPT.jsx
+++ frontend/cherish/src/components/STTandGPT/STTGPT.jsx
@@ -4,6 +4,39 @@ import { useSpeechRecognition } from "react-speech-kit";
 var canCheck = true;
 
 function STTGPT() {
+  // fetch
+  const fetchTest = () => {
+    const messages = [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: `${keywords}가 뭔지 간단하게 설명해줘.` },
+    ];
+
+    // fetch("http://localhost:8080/meeting/chat", {
+    fetch("https://i10d103.p.ssafy.io/meeting/chat", {
+      method: "POST", // HTTP 메소드를 POST로 설정
+      headers: {
+        Accept: "*/*", // 응답 데이터 타입
+        "Content-Type": "application/json", // 콘텐츠 타입을 application/json으로 지정
+      },
+      body: JSON.stringify({
+        // 요청 바디에 JSON 데이터를 문자열로 변환하여 전달
+        id: 0,
+        kakaoId: 0,
+        nickname: "string",
+        meetingId: 3,
+        content: "서버 api 작동 테스트",
+        createdAt: "string",
+      }),
+    })
+      .then((response) => console.log(response)) // 응답을 JSON으로 변환
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error); // 오류 처리
+      });
+  };
+
   // STT
   const [recogString, setRecogString] = useState("");
   const { listen, listening, stop } = useSpeechRecognition({
@@ -28,44 +61,48 @@ function STTGPT() {
       setRecogString(result);
     },
   });
-  
+
   // GPT
-  const [apiKey, setApiKey] = useState('');
-  const [keywords, setKeywords] = useState('');
-  const [result, setResult] = useState('');
+  const [apiKey, setApiKey] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const [result, setResult] = useState("");
   const search = () => {
     const messages = [
-      { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: `${keywords}가 뭔지 간단하게 설명해줘.` },
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: `${keywords}가 뭔지 간단하게 설명해줘.` },
     ];
 
-    fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST', // HTTP 메소드를 POST로 설정
+    fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST", // HTTP 메소드를 POST로 설정
       headers: {
-        'Authorization': `Bearer ${apiKey}`, // API 키를 포함한 인증 헤더
-        'Content-Type': 'application/json', // 콘텐츠 타입을 application/json으로 지정
+        Authorization: `Bearer ${apiKey}`, // API 키를 포함한 인증 헤더
+        "Content-Type": "application/json", // 콘텐츠 타입을 application/json으로 지정
       },
-      body: JSON.stringify({ // 요청 바디에 JSON 데이터를 문자열로 변환하여 전달
-        model: 'gpt-3.5-turbo',
+      body: JSON.stringify({
+        // 요청 바디에 JSON 데이터를 문자열로 변환하여 전달
+        model: "gpt-3.5-turbo",
         temperature: 0.5,
         n: 1,
         messages: messages,
       }),
     })
-      .then(response => response.json()) // 응답을 JSON으로 변환
-      .then(data => {
+      .then((response) => response.json()) // 응답을 JSON으로 변환
+      .then((data) => {
         const output = data.choices
-          .map(choice => choice.message.content.split('\n').join('<br/>'))
-          .join('');
+          .map((choice) => choice.message.content.split("\n").join("<br/>"))
+          .join("");
         setResult(output); // 결과를 설정
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error); // 오류 처리
       });
   };
 
   return (
     <div className="container">
+      <button onClick={fetchTest}>fetch test</button>
+      <br />
+      <br />
       <p>{recogString}</p>
       {listening && <div>음성인식 활성화 중</div>}
       <button onMouseDown={listen} onMouseUp={stop}>
