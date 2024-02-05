3edc97f3dd6cd994032650b30bb5548c6d1296ec
unknown <1025jy@naver.com>
2024-02-05 21:56:00 +0900

    Feat : qna mapper, service 추가
    이슈 코드 : [S10P12D103]

--- backend/cherish/src/main/java/com/ssafy/cherish/qna/controller/QnaController.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/qna/controller/QnaController.java
@@ -1,4 +1,49 @@
 package com.ssafy.cherish.qna.controller;
 
+import com.ssafy.cherish.qna.model.dto.QuestionDto;
+import com.ssafy.cherish.qna.model.service.QnaService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.MarshalledObject;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/qna")
+@Slf4j
 public class QnaController {
+    @Autowired
+    private QnaService qnaService;
+
+    @GetMapping("/getQuestion")
+    @Operation(summary = "오늘의 질문 불러오기", description = "현재 coupledto에 저장되어 있는 질문 번호와 맞는 질문을 불러온다.")
+    public ResponseEntity<?> getQuestion (@RequestParam int coupleId) {
+        log.debug("getQuestion 호출 : {}", coupleId);
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        HttpStatus status;
+
+        try {
+            int id = qnaService.getQuestionCnt(coupleId);
+            QuestionDto questionDto = qnaService.getQuestion(id);
+
+            resultMap.put("qustionDto", questionDto);
+            status = HttpStatus.OK;
+
+            return new ResponseEntity<Map<String, Object>>(resultMap, status);
+        } catch (Exception e) {
+            log.error("getQuestion 에러 : {}", e.getMessage());
+            resultMap.put("getQuestion 에러", e.getMessage());
+            status = HttpStatus.BAD_REQUEST;
+
+            return new ResponseEntity<Map<String, Object>>(resultMap, status);
+        }
+    }
+
+
+
 }
--- backend/cherish/src/main/java/com/ssafy/cherish/qna/model/mapper/QnaMapper.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/qna/model/mapper/QnaMapper.java
@@ -11,16 +11,17 @@ import java.util.Map;
 @Mapper
 public interface QnaMapper {
 
+    // couple 테이블에 있는 question_cnt 가져오기
     int getQuestionCnt (int coupleId) throws SQLException;
-
+    // 가져온 question_cnt로 question 테이블 가져오기
     QuestionDto getQuestion (int questionId) throws SQLException;
-
+    // 사용자가 찍은 영상의 filepath값을 answer 테이블 생성 후 넣기
     void saveAnswer (AnswerDto answerDto) throws SQLException;
-
+    // 커플 두 명이 모두 answer를 찍었는지 확인
     int chkAnswer (Map<String, Object> map) throws SQLException;
-
+    // 특정 answer 영상 보기
     AnswerDto getAnswer (Map<String, Object> map) throws SQLException;
-
+    // 영상을 찍은 질문 리스트 보여주기
     List<AnswerDto> getAnswerList (int coupleId) throws SQLException;
 
 }
