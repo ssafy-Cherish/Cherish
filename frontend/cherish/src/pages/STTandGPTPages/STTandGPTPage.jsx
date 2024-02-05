f3851134d55a270756f8349305b8070daffd23b9
이승준 <tmdwns7809@gmail.com>
2024-02-05 19:27:18 +0900

    Feat : STTGPT 기능 구현중
    이슈 코드 : []

--- frontend/cherish/src/pages/STTandGPTPages/STTandGPTPage.jsx
+++ frontend/cherish/src/pages/STTandGPTPages/STTandGPTPage.jsx
@@ -1,12 +1,10 @@
-import Meeting2 from "../../components/STTandGPT/Meeting2"
-import STTGPT from "../../components/STTandGPT/STTGPT"
+import STTGPT from "../../components/STTandGPT/STTGPT";
 function STTandGPTPage() {
-
   return (
     <>
       <STTGPT />
     </>
-  )
+  );
 }
 
-export default STTandGPTPage
+export default STTandGPTPage;
