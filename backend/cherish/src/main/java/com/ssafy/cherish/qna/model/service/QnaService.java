80e7f4dd5314e7cf69457b7546ee977a739bd1cf
김준엽 <wnsduq1025@gmail.com>
2024-02-05 21:56:00 +0900

    Feat : qna mapper, service 추가
    이슈 코드 : [S10P12D103]

--- backend/cherish/src/main/java/com/ssafy/cherish/qna/model/service/QnaService.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/qna/model/service/QnaService.java
@@ -1,4 +1,19 @@
 package com.ssafy.cherish.qna.model.service;
 
+import com.ssafy.cherish.qna.model.dto.AnswerDto;
+import com.ssafy.cherish.qna.model.dto.QuestionDto;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 public interface QnaService {
+
+    int getQuestionCnt (int coupleId) throws SQLException;
+    QuestionDto getQuestion (int questionId) throws SQLException;
+    void saveAnswer (AnswerDto answerDto) throws SQLException;
+    int chkAnswer (Map<String, Object> map) throws SQLException;
+    AnswerDto getAnswer (Map<String, Object> map) throws SQLException;
+    List<AnswerDto> getAnswerList (int coupleId) throws SQLException;
+
 }
