658cc106b65e50ee130b078694fac6adf052993d
전찬혁 <cksgur5530@gmail.com>
2024-02-05 12:54:03 +0900

    Fix : kakao API 정리
    이슈 코드 : []
    
    - 코드 정리만 했습니다.

--- frontend/cherish/src/pages/DiaryPages/DiaryYearlyPage.jsx
+++ frontend/cherish/src/pages/DiaryPages/DiaryYearlyPage.jsx
@@ -17,72 +17,72 @@ import Month11 from "../../assets/diary/Month/Month11.svg";
 import Month12 from "../../assets/diary/Month/Month12.svg";
 
 const MonthArr = [
-	Month1,
-	Month2,
-	Month3,
-	Month4,
-	Month5,
-	Month6,
-	Month7,
-	Month8,
-	Month9,
-	Month10,
-	Month11,
-	Month12,
+  Month1,
+  Month2,
+  Month3,
+  Month4,
+  Month5,
+  Month6,
+  Month7,
+  Month8,
+  Month9,
+  Month10,
+  Month11,
+  Month12,
 ];
 
 const DiaryYearlyPage = () => {
-	const navigate = useNavigate();
-	const [searchParams, setSearchParams] = useSearchParams();
-	const [scope, animate] = useAnimate();
-	let year = searchParams.get("year");
-	if (!year) year = new Date().getFullYear();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [scope, animate] = useAnimate();
+  let year = searchParams.get("year");
+  if (!year) year = new Date().getFullYear();
 
-	function moveYear(move) {
-		animate("#year", { y: [0, -5, 5, 0] }, { duration: 0.2 });
-		animate("#month", { opacity: [1, 0, 1] });
-		year = +year + move;
-		searchParams.set("year", year);
-		setSearchParams(searchParams, { replace: true });
-	}
+  function moveYear(move) {
+    animate("#year", { y: [0, -5, 5, 0] }, { duration: 0.2 });
+    animate("#month", { opacity: [1, 0, 1] });
+    year = +year + move;
+    searchParams.set("year", year);
+    setSearchParams(searchParams, { replace: true });
+  }
 
-	function moveToMonth(month) {
-		navigate({ pathname: "/diary/month", search: `?year=${year}&month=${month}` });
-	}
+  function moveToMonth(month) {
+    navigate({ pathname: "/diary/month", search: `?year=${year}&month=${month}` });
+  }
 
-	return (
-		<Modal z={1} modalcss="h-[90vh] w-[70vw]" isX={false} nav="/">
-			<div
-				className="flex flex-col  absolute h-[40vw] ml-[12vw] mt-[2vw] w-[45vw] items-center"
-				ref={scope}
-			>
-				<div className="text-[2vw] text-red-400 mb-[2vw] ">
-					<button onClick={() => moveYear(-1)}>&lt;</button>
-					<div id="year" className="inline-block text-[2vw]">
-						&nbsp;{year}&nbsp;
-					</div>
-					<button onClick={() => moveYear(1)}>&gt;</button>
-				</div>
-				<div className="grid grid-rows-4 grid-cols-3 w-[35vw] h-[40vw]">
-					{MonthArr.map((src, i) => (
-						<div key={src} className="col-span-1 row-span-1 w-[8vw]">
-							<motion.img
-								id="month"
-								src={src}
-								alt={src}
-								whileHover={{ scale: 1.2 }}
-								className="ml-[2vw] w-full h-full"
-								onClick={() => moveToMonth(i + 1)}
-							/>
-						</div>
-					))}
-				</div>
-			</div>
-			<div>
-				<img className="w-full" src={yearlyImg} alt="" />
-			</div>
-		</Modal>
-	);
+  return (
+    <Modal z={1} modalcss="h-[90vh] w-[70vw]" isX={false} nav="/">
+      <div
+        className="flex flex-col  absolute h-[40vw] ml-[12vw] mt-[2vw] w-[45vw] items-center"
+        ref={scope}
+      >
+        <div className="text-[2vw] text-red-400 mb-[2vw] ">
+          <button onClick={() => moveYear(-1)}>&lt;</button>
+          <div id="year" className="inline-block text-[2vw]">
+            &nbsp;{year}&nbsp;
+          </div>
+          <button onClick={() => moveYear(1)}>&gt;</button>
+        </div>
+        <div className="grid grid-rows-4 grid-cols-3 w-[35vw] h-[40vw]">
+          {MonthArr.map((src, i) => (
+            <div key={src} className="col-span-1 row-span-1 w-[8vw]">
+              <motion.img
+                id="month"
+                src={src}
+                alt={src}
+                whileHover={{ scale: 1.2 }}
+                className="ml-[2vw] w-full h-full hover:cursor-pointer"
+                onClick={() => moveToMonth(i + 1)}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <img className="w-full" src={yearlyImg} alt="" />
+      </div>
+    </Modal>
+  );
 };
 
 export default DiaryYearlyPage;
