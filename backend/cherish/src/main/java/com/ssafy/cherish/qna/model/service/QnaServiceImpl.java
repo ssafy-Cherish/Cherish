852abb656c179ebd4e7ac6394cba6fe78930bc58
김준엽 <wnsduq1025@gmail.com>
2024-02-06 17:48:53 +0900

    Feat : QnaController 및 QnaMapper 추가
    이슈 코드 : [S10P12D103]

--- backend/cherish/src/main/java/com/ssafy/cherish/qna/model/service/QnaServiceImpl.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/qna/model/service/QnaServiceImpl.java
@@ -4,8 +4,15 @@ import com.ssafy.cherish.qna.model.dto.AnswerDto;
 import com.ssafy.cherish.qna.model.dto.QuestionDto;
 import com.ssafy.cherish.qna.model.mapper.QnaMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -15,34 +22,70 @@ public class QnaServiceImpl implements QnaService {
 
     @Autowired
     private QnaMapper qnaMapper;
+    @Value("${custom.path.answer}")
+    private String answerPath;
 
     @Override
-    public int getQuestionCnt(int coupleId) throws SQLException {
+    public int getQuestionCnt(int coupleId) throws Exception {
         return qnaMapper.getQuestionCnt(coupleId);
     }
 
     @Override
-    public QuestionDto getQuestion(int questionId) throws SQLException {
+    public QuestionDto getQuestion(int questionId) throws Exception {
         return qnaMapper.getQuestion(questionId);
     }
 
     @Override
-    public void saveAnswer(AnswerDto answerDto) throws SQLException {
-        qnaMapper.saveAnswer(answerDto);
+    @Transactional(rollbackFor = Exception.class)
+    public int saveAnswer(MultipartFile answer, Map<String, Object> map) throws Exception {
+        AnswerDto answerDto = new AnswerDto();
+
+        answerDto.setCoupleId(Integer.parseInt((String)map.get("coupleId")));
+        answerDto.setNickname((String)map.get("nickname"));
+        answerDto.setQuestionId(Integer.parseInt((String)map.get("questionId")));
+        answerDto.setKakaoId(Long.parseLong((String)map.get("kakaoId")));
+
+        qnaMapper.createAnswer(answerDto);
+
+        String answerPath = setAnswerDir(answerDto);
+
+        answerDto.setFilepath(answerPath);
+
+        return qnaMapper.updateAnswerPath(answerDto);
     }
 
     @Override
-    public int chkAnswer(Map<String, Object> map) throws SQLException {
-        return qnaMapper.chkAnswer(map);
+    public boolean chkAnswer(Map<String, Object> map) throws Exception {
+        int cnt = qnaMapper.chkAnswer(map);
+
+        if (cnt == 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public AnswerDto getAnswer(Map<String, Object> map) throws SQLException {
+    public AnswerDto getAnswer(Map<String, Object> map) throws Exception {
         return qnaMapper.getAnswer(map);
     }
 
     @Override
-    public List<AnswerDto> getAnswerList(int coupleId) throws SQLException {
+    public List<AnswerDto> getAnswerList(int coupleId) throws Exception {
         return qnaMapper.getAnswerList(coupleId);
     }
+
+    String setAnswerDir (AnswerDto answerDto) throws Exception {
+        String uploadDir = answerPath + answerDto.getId() + File.separator;
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        String resPath = uploadDir + answerDto.getId() + "_" + answerDto.getQuestionId() + "_" + ".mp4";
+
+        return resPath;
+    }
 }
+
