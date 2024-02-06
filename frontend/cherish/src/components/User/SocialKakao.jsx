d8a9d79b45acc9aa3c214cdd72a5b681d767e000
전찬혁 <cksgur5530@gmail.com>
2024-02-06 09:22:35 +0900

    Feat : UserStore에 userId  저장
    이슈 코드 : [S10P12D103-224]
    
    - UserStore 수정
      - userId 저장

--- frontend/cherish/src/components/User/SocialKakao.jsx
+++ frontend/cherish/src/components/User/SocialKakao.jsx
@@ -9,107 +9,103 @@ import UserStore from "../../stores/UserStore";
 import CoupleStore from "../../stores/CoupleStore";
 
 const SocialKakao = () => {
-	const navigate = useNavigate();
-	const [openWaitingModal, setOpenWaitingModal] = useState(false);
+  const navigate = useNavigate();
+  const [openWaitingModal, setOpenWaitingModal] = useState(false);
 
-	const { setUserInfo } = UserStore();
-	const { setCoupleInfo } = CoupleStore();
+  const { setUserInfo } = UserStore();
+  const { setCoupleInfo } = CoupleStore();
 
-	const [coupleCode, setCoupleCode] = useState("");
-	const [preventClick, setPreventClick] = useState(false);
+  const [coupleCode, setCoupleCode] = useState("");
+  const [preventClick, setPreventClick] = useState(false);
 
-	function handleKakaoLogin() {
-		async function fetchKakaoLogin() {
-			const response = await fetch(`${import.meta.env.VITE_APP_SERVER_URL}/user/login`, {
-				headers: {
-					Authorization: Kakao.Auth.getAccessToken(),
-				},
-			});
-			const resData = await response.json();
-			if (!response.ok) {
-				throw Error("login fetch Error");
-			}
+  function handleKakaoLogin() {
+    async function fetchKakaoLogin() {
+      const response = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}/user/login`, {
+        headers: {
+          Authorization: Kakao.Auth.getAccessToken(),
+        },
+      });
+      const resData = await response.json();
+      if (!response.ok) {
+        throw Error("login fetch Error");
+      }
 
-			if (resData.verified) {
-				if (!resData.coupleDto.coupled) {
-					setCoupleCode(resData.coupleDto.code);
-					setOpenWaitingModal(true);
-				} else {
-					setUserInfo(resData.kakao_id, resData.nickname);
-					const couple = resData.coupleDto;
-					setCoupleInfo(
-						couple.id,
-						couple.code,
-						couple.user1,
-						couple.user2,
-						couple.anniversary,
-						resData.birthdays
-					);
-					navigate("/");
-				}
-			} else {
-				navigate("/user/signup");
-			}
+      if (resData.verified) {
+        if (!resData.coupleDto.coupled) {
+          setCoupleCode(resData.coupleDto.code);
+          setOpenWaitingModal(true);
+        } else {
+          setUserInfo(resData.kakao_id, resData.nickname, resData.user_id);
+          const couple = resData.coupleDto;
+          setCoupleInfo(
+            couple.id,
+            couple.code,
+            couple.user1,
+            couple.user2,
+            couple.anniversary,
+            resData.birthdays
+          );
+          navigate("/");
+        }
+      } else {
+        navigate("/user/signup");
+      }
 
-			return resData;
-		}
+      return resData;
+    }
 
-		if (preventClick) return;
-		setPreventClick(true);
+    if (preventClick) return;
+    setPreventClick(true);
 
-		Kakao.Auth.login({
-			success() {
-				fetchKakaoLogin();
+    Kakao.Auth.login({
+      success() {
+        fetchKakaoLogin();
 
-				setPreventClick(false);
-			},
-			fail(err) {
-				console.log(err);
-				return;
-			},
-		});
-	}
-	function handleKakaoLogout() {
-		if (!Kakao.Auth.getAccessToken()) {
-			alert("로그인중이 아닙니다.");
-			return;
-		}
-		Kakao.Auth.logout(() => {
-			alert("로그아웃");
-		});
-	}
+        setPreventClick(false);
+      },
+      fail(err) {
+        console.log(err);
+        return;
+      },
+    });
+  }
+  function handleKakaoLogout() {
+    if (!Kakao.Auth.getAccessToken()) {
+      alert("로그인중이 아닙니다.");
+      return;
+    }
+    Kakao.Auth.logout(() => {
+      alert("로그아웃");
+    });
+  }
 
-	function unlinkUser() {
-		if (!Kakao.Auth.getAccessToken()) {
-			alert("로그인중이 아닙니다.");
-			return;
-		}
+  function unlinkUser() {
+    if (!Kakao.Auth.getAccessToken()) {
+      alert("로그인중이 아닙니다.");
+      return;
+    }
 
-		Kakao.API.request({
-			url: "/v1/user/unlink",
-			success(res) {
-				console.log(res);
-			},
-			fail(err) {
-				console.log(err);
-			},
-		});
-	}
+    Kakao.API.request({
+      url: "/v1/user/unlink",
+      success(res) {
+        console.log(res);
+      },
+      fail(err) {
+        console.log(err);
+      },
+    });
+  }
 
-	return (
-		<>
-			<img
-				src={KakaoLogin}
-				onClick={handleKakaoLogin}
-				className="w-[80%] hover:cursor-pointer"
-			/>
-			<AnimatePresence>
-				{openWaitingModal && (
-					<WaitingModal onClose={() => setOpenWaitingModal(false)} code={coupleCode} />
-				)}
-			</AnimatePresence>
-		</>
-	);
+  return (
+    <>
+      <img src={KakaoLogin} onClick={handleKakaoLogin} className="w-[80%] hover:cursor-pointer" />
+      <AnimatePresence>
+        {openWaitingModal && (
+          <WaitingModal onClose={() => setOpenWaitingModal(false)} code={coupleCode} />
+        )}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default SocialKakao;
