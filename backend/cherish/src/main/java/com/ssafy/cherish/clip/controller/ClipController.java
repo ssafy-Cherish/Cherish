8cf44a1974ed693f8a0d6f61c1968bdb8ee035d8
김예현 <qkdo12@naver.com>
2024-02-07 15:43:34 +0900

    Feat : ClipService와 ClipController에서 비동기 처리되도록 로직 분리
    이슈 코드 : []

--- backend/cherish/src/main/java/com/ssafy/cherish/clip/controller/ClipController.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/clip/controller/ClipController.java
@@ -6,16 +6,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/clip")
@@ -23,6 +27,9 @@ import java.util.Map;
 public class ClipController {
     @Autowired
     private ClipService clipService;
+
+    @Value("${custom.path.clip}")
+    private String clipPath;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "클립 저장", description = "키워드가 포함된 두 명의 동영상 저장하기위해 파일 두개 입력받음")
@@ -34,11 +41,64 @@ public class ClipController {
     ) {
         log.debug("클립 입력 : {}", map.toString());
         try {
-            String res = clipService.saveClip(clip1, clip2, map);
-            return new ResponseEntity(res,HttpStatus.CREATED);
+            ClipDto clipDto = new ClipDto();
+
+            clipDto.setMeetingId(Integer.parseInt((String) map.get("meeting_id")));
+            clipDto.setKeyword((String) map.get("keyword"));
+
+            clipService.createClip(clipDto);
+
+            log.info("saveClip 중 생성된 filepath 빈 객체 : {}", clipDto.toString());
+
+            // 클립 병합 시 필요한 경로 설정
+            // String[] {leftVideoPath,rightVideoPath,resPath,resFile};
+            String[] pathForMerge = setClipDir(clipDto);
+            log.info(Arrays.toString(pathForMerge));
+
+            //클립 임시 저장
+            clip1.transferTo(new File(pathForMerge[0]));
+            clip2.transferTo(new File(pathForMerge[1]));
+
+            // 클립 저장 작업을 비동기적으로 처리
+            CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+                try {
+                    return clipService.saveClip(clipDto,pathForMerge);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("동영상 처리 중 문제 발생 : " + e.getMessage());
+                }
+            });
+
+            // 비동기 작업이 완료되면 결과를 받아와서 출력
+            future.thenAccept(result -> {
+                System.out.println("비동기 작업 결과: " + result);
+            });
+
+            // 클립 저장이 완료되지 않았어도 즉시 응답 반환
+            return ResponseEntity.ok("클립 저장 요청이 성공적으로 접수되었습니다.");
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity("동영상 처리 중 문제 발생 : "+e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    String[] setClipDir(ClipDto clipDto) throws Exception {
+        // 클립 저장 경로 설정
+        String uploadDir = clipPath + clipDto.getMeetingId() + File.separator;
+        Path uploadPath = Paths.get(uploadDir);
+
+
+        // 디렉토리가 없으면 생성
+        if (!Files.exists(uploadPath)) {
+            //여기서 IOException 발생 가능성 있음
+            Files.createDirectories(uploadPath); // 디렉토리 생성
+        }
+
+        // 파일명 생성
+        String leftVideoPath = uploadDir + clipDto.getId() + "_" + clipDto.getMeetingId() + "_" + clipDto.getKeyword() + "templeft.webm";
+        String rightVideoPath = uploadDir + clipDto.getId() + "_" + clipDto.getMeetingId() + "_" + clipDto.getKeyword() + "tempright.webm";
+        String resPath = uploadDir + clipDto.getId() + "_" + clipDto.getMeetingId() + "_" + clipDto.getKeyword() + ".webm";
+        String resFile=clipDto.getId() + "_" + clipDto.getMeetingId() + "_" + clipDto.getKeyword() + ".webm";
+
+        return new String[]{leftVideoPath, rightVideoPath, resPath,resFile};
     }
 }
