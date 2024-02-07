329c5db843acf085e1b5b9b2b9d8ffdbc5adeb6b
김준엽 <1025jy@naver.com>
2024-02-07 20:41:29 +0900

    Feat : videoController 추가
    이슈 코드 : [S10P12D103]

---
+++ backend/cherish/src/main/java/com/ssafy/cherish/clip/model/service/VideoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ssafy.cherish.clip.model.service;
+
+import com.ssafy.cherish.clip.model.dto.VideoDto;
+import com.ssafy.cherish.clip.model.mapper.VideoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class VideoServiceImpl implements VideoService {
+
+    @Autowired
+    private VideoMapper videoMapper;
+
+    @Override
+    public List<VideoDto> getVideoList(Map<String, Object> map) throws Exception {
+        return videoMapper.getVideoList(map);
+    }
+}
