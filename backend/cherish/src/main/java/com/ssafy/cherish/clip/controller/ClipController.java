4ad184a19b99e4ed3734d36f520253876bd1c973
김예현 <qkdo12@naver.com>
2024-02-07 17:47:12 +0900

    Fix: 클립 저장 비동기 처리 후 클라이언트에 다시 클립 url 전송
    이슈 코드 : []

--- backend/cherish/src/main/java/com/ssafy/cherish/clip/controller/ClipController.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/clip/controller/ClipController.java
@@ -2,9 +2,11 @@ package com.ssafy.cherish.clip.controller;
 
 import com.ssafy.cherish.clip.model.dto.ClipDto;
 import com.ssafy.cherish.clip.model.service.ClipService;
+import com.ssafy.cherish.utils.SocketHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -27,16 +29,22 @@ import java.util.concurrent.CompletableFuture;
 public class ClipController {
     @Autowired
     private ClipService clipService;
-
+    private final SocketHandler socketHandler;
     @Value("${custom.path.clip}")
     private String clipPath;
+
+    @Autowired
+    public ClipController(SocketHandler socketHandler) {
+        this.socketHandler = socketHandler;
+    }
+
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "클립 저장", description = "키워드가 포함된 두 명의 동영상 저장하기위해 파일 두개 입력받음")
     public ResponseEntity saveClip(
             @RequestPart("clip1") MultipartFile clip1,
             @RequestPart("clip2") MultipartFile clip2,
-            @Parameter(name = "클립 파일 저장에 필요한 정보 map", description = "meeting_id,keyword")
+            @Parameter(name = "클립 파일 저장에 필요한 정보 map", description = "meeting_id,couple_id,keyword")
             @RequestParam Map<String, Object> map
     ) {
         log.debug("클립 입력 : {}", map.toString());
@@ -70,9 +78,10 @@ public class ClipController {
             });
 
             // 비동기 작업이 완료되면 결과를 받아와서 출력
-            future.thenAccept(result -> {
-                System.out.println("비동기 작업 결과: " + result);
-            });
+            future.thenAccept(url -> {
+                log.info("데이터 url : {}",url);
+                socketHandler.sendClipUrl(Integer.parseInt((String) map.get("couple_id")),url);
+            }).join();
 
             // 클립 저장이 완료되지 않았어도 즉시 응답 반환
             return ResponseEntity.ok("클립 저장 요청이 성공적으로 접수되었습니다.");
