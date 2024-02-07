fea24a2210f438c31c3092c01f0061bcb526f4fa
전찬혁 <cksgur5530@gmail.com>
2024-02-08 02:06:08 +0900

    Feat : 메인페이지 네비게이션 가드 추
    이슈 코드 : [S10P12D103-267]
    
    - rootLayout 접근시 로그인을 확인하여 안되어 있다면 login 페이지로
      redirect

--- frontend/cherish/src/pages/RootLayout.jsx
+++ frontend/cherish/src/pages/RootLayout.jsx
@@ -1,18 +1,22 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import NavBar from "../components/Common/NavBar";
 import useUserStore from "../stores/useUserStore";
 
 export default function RootLayout() {
 	const { userId } = useUserStore();
-	const navigate = useNavigate();
-	if (!userId) navigate("/user");
 
 	return (
-		<div className="grid grid-cols-mainLayout gap-5">
-			<NavBar />
-			<div className="col-span-10 mx-5">
-				<Outlet />
-			</div>
-		</div>
+		<>
+			{userId ? (
+				<div className="grid grid-cols-mainLayout gap-5">
+					<NavBar />
+					<div className="col-span-10 mx-5">
+						<Outlet />
+					</div>
+				</div>
+			) : (
+				<Navigate to="/user" replace />
+			)}
+		</>
 	);
 }
