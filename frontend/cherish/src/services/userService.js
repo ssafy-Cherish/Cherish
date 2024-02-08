1e8009f0773392038c549ee01c6b315891057d73
전찬혁 <cksgur5530@gmail.com>
2024-02-08 21:17:40 +0900

    Feat : 로그아웃 및 카카오 메시지 code 자동 연결
    이슈 코드 : [S10P12D103-267]
    
    - 웹 아이콘 변경 체리로고
    - 로그아웃 연결
    - 카카오 메시지 공유를 통해 접근하면 code 자동 활성화

---
+++ frontend/cherish/src/services/userService.js
@@ -0,0 +1,45 @@
+import Kakao, { getAccessToken } from "../utils/Kakao";
+
+export async function checkByCode(code) {
+	const res = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}/user/code?code=${code}`);
+	const resData = await res.json();
+
+	if (!res.ok) {
+		throw new Error("checkByCode Fetch Error occured!");
+	}
+
+	return resData;
+}
+
+export async function kakaoLoginFetch() {
+	const response = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}/user/login`, {
+		headers: {
+			Authorization: getAccessToken(),
+		},
+	});
+	const resData = await response.json();
+
+	if (!response.ok) {
+		throw Error("login fetch Error");
+	}
+
+	return resData;
+}
+
+export async function userDeleteFetch(userId) {
+	const res = await fetch(`${import.meta.env.VITE_APP_BACKEND_URL}/user/delete/${userId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: getAccessToken(),
+		},
+	});
+
+	const resData = await res.json();
+
+	if (!res.ok) {
+		new Error("userDelete Fetch Error");
+	}
+
+	return resData;
+}
