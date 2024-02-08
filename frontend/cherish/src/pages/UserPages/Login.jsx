e9fe2454fd78c853211439a176649ff5f3fbaa06
전찬혁 <cksgur5530@gmail.com>
2024-02-08 12:17:20 +0900

    Fix : login 리다이렉트 위치 변경
    이슈 코드 : []

--- frontend/cherish/src/pages/UserPages/Login.jsx
+++ frontend/cherish/src/pages/UserPages/Login.jsx
@@ -1,25 +1,15 @@
-import { Navigate } from "react-router-dom";
 import SocialKakao from "../../components/User/SocialKakao";
-import useUserStore from "../../stores/useUserStore";
 
 const Login = () => {
-  const { userId } = useUserStore();
-
   return (
-    <>
-      {userId ? (
-        <Navigate to="/" />
-      ) : (
-        <div className="flex justify-center items-center h-full text-[2vw]">
-          <div className="bg-pink h-[50%] w-[80%] flex flex-col items-center rounded-xl shadow-2xl">
-            <div className=" p-[8%]">로그인</div>
-            <div className="grow flex justify-center items-center pb-[10%]">
-              <SocialKakao />
-            </div>
-          </div>
+    <div className="flex justify-center items-center h-full text-[2vw]">
+      <div className="bg-pink h-[50%] w-[80%] flex flex-col items-center rounded-xl shadow-2xl">
+        <div className=" p-[8%]">로그인</div>
+        <div className="grow flex justify-center items-center pb-[10%]">
+          <SocialKakao />
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
