47c285a5a4e57e69830815e97095e343a43db749
김경범 <gung2227@naver.com>
2024-02-08 09:49:14 +0900

    - Fix : 몇 몇 카드 애니메이션 버그 수정 및 메인페이지 디자인 (폰트 색상,   스타일 수정)
    이슈 코드 : []
    
    - 메인페이지 D-day 현재날짜와 기념일 계산하여 표시
    - 화분카드, 다이어리카드, 체리콜 카드 폰트 컬러, 스타일 변경
    - 오늘의 질문 카드 폰트컬러, 스타일 변경
    - 오늘의 질문, 체리박스 질문카드, 날짜선택 셀렉트 애니메이션 버그 수정
    - 캐러셸 테두리 추가

--- frontend/cherish/src/components/Main/PotCard.jsx
+++ frontend/cherish/src/components/Main/PotCard.jsx
@@ -2,14 +2,14 @@ import HeartIcon from "../../assets/Common/HeartIcon.svg";
 import PotLv1 from "../../assets/Pot/PotLv1.svg";
 
 
-export default function PotCard() {
+export default function PotCard({dday}) {
   return (
     <>
       <div id="PotState" className="col-span-2 flex flex-col items-center">
         <div id="PotImg">
           <img src={PotLv1} alt="PotImg" />
         </div>
-        <p id="PotName" className="text-[1.2vw] text-cherry">
+        <p id="PotName" className="text-[1.2vw] text-cherry font-bold">
           체리 떡잎
         </p>
         <progress
@@ -23,18 +23,18 @@ export default function PotCard() {
         id="PotDescrption"
         className="h-[15vw] mt-[2vw] col-span-3 flex flex-col items-center justify-around"
       >
-        <p id="PotCouple" className="text-[1.5vw]">
+        <p id="PotCouple" className="text-[1.5vw] font-bold text-text-black">
           알콩달콩 <span className="text-cherry">커플</span>
         </p>
         <div id="PotCoupleName" className="flex flex-row items-center">
-          <p className="text-[1.2vw]">나희도</p>
+          <p className="text-[1.2vw] text-text-black">나희도</p>
           <div>
             <img src={HeartIcon} alt="HeartIcon" />
           </div>
-          <p className="text-[1.2vw]">백이진</p>
+          <p className="text-[1.2vw] text-text-black">백이진</p>
         </div>
-        <p className="text-[1.2vw]">D + 99</p>
-        <button id="GoPot" className="text-[1.2vw]">
+        <p className="text-[1.2vw] text-text-black">D + {dday}</p>
+        <button id="GoPot" className="text-[1.2vw] text-text-black">
           경험치 기록 보기
         </button>
       </div>
--- frontend/cherish/src/components/TodayQuestion/Question.jsx
+++ frontend/cherish/src/components/TodayQuestion/Question.jsx
@@ -6,7 +6,7 @@ import Answer from "./Answer";
 export default function Question({ reply }) {
   const [isOpened, setIsOpened] = useState(false);
   let questioncss = `transition-[height] duration-500 w-[42vw] bg-white mt-[1vw] rounded-[15px] text-[1vw] shadow-md ${
-    isOpened && reply ? "h-[25vw] flex flex-col" : "h-[4.2vw]"
+    isOpened && reply ? "h-[25vw] flex flex-col overflow-y-auto" : "h-[4.2vw]"
   }`;
 
   const handleClickQuestionButton = () => {
@@ -16,7 +16,7 @@ export default function Question({ reply }) {
   };
   return (
     <>
-      <div className={questioncss}>
+      <div id='question' className={questioncss}>
         <div className="grid grid-cols-10">
           <div className="col-span-9 ml-[1vw] mt-[0.2vw] py-[0.5vw]">
             <p>20XX년 X월 X일</p>
