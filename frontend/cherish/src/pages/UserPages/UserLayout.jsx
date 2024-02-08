8243878e26e9a9b8b7fab9480b4238e9273e5d6e
전찬혁 <cksgur5530@gmail.com>
2024-02-08 12:17:20 +0900

    Fix : login 리다이렉트 위치 변경
    이슈 코드 : []

--- frontend/cherish/src/pages/UserPages/UserLayout.jsx
+++ frontend/cherish/src/pages/UserPages/UserLayout.jsx
@@ -1,20 +1,26 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Intro from "../../components/User/Intro";
-import { useEffect } from "react";
+import useUserStore from "../../stores/useUserStore";
 
 const UserLayout = () => {
-	return (
-		<>
-			<div className="grid grid-cols-8 gap-5 h-[100vh]">
-				<div className="col-span-5">
-					<Intro></Intro>
-				</div>
-				<div className="col-span-3">
-					<Outlet></Outlet>
-				</div>
-			</div>
-		</>
-	);
+  const { userId } = useUserStore();
+
+  return (
+    <>
+      {userId ? (
+        <Navigate to="/" replace />
+      ) : (
+        <div className="grid grid-cols-8 gap-5 h-[100vh]">
+          <div className="col-span-5">
+            <Intro></Intro>
+          </div>
+          <div className="col-span-3">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default UserLayout;
