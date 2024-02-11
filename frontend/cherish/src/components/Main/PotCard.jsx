58c8120c8c46c279d6e1bc5deab113a73fb4a25c
김경범 <gung2227@naver.com>
2024-02-11 21:06:38 +0900

    - Feat : 화분 클립개수, 화분레벨 기능추가 및 api 연결, 체리박스 기능   추가 api연결, 메모추가
    이슈 코드 : [S10P12D103-247]
    
    - 화분페이지 경험치, 레벨 api 연결
    - 클립 총 개수, 도넛 그래프 컴포넌트화
    - 체리박스 api 연결
    - 날짜별로 볼 수 있도록 기능 구현
    - 특정 키워드 박스 클릭시 해당 키워드 동영상으로 이동 기능 구현
    - 메인페이지 메모 기능 추가
    - 다이어리에 있는 메모를 참고하여 기능 구현

--- frontend/cherish/src/components/Main/PotCard.jsx
+++ frontend/cherish/src/components/Main/PotCard.jsx
@@ -1,8 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 import HeartIcon from "../../assets/Common/HeartIcon.svg";
 import PotLv1 from "../../assets/Pot/PotLv1.svg";
+import useCoupleStore from "../../stores/useCoupleStore";
+import { fetchExpLevel } from "../../services/PotService";
+
 
 
 export default function PotCard({dday}) {
+  const {coupleId, userInfos} = useCoupleStore()
+  const { data: expLevel, isLoading } = useQuery({
+    queryKey: ["expLevel", coupleId],
+    queryFn: () => fetchExpLevel(coupleId),
+  });
   return (
     <>
       <div id="PotState" className="col-span-2 flex flex-col items-center">
@@ -12,12 +21,12 @@ export default function PotCard({dday}) {
         <p id="PotName" className="text-[1.2vw] text-cherry font-bold">
           체리 떡잎
         </p>
-        <progress
+        {isLoading ? <div>로딩중</div> : <progress
           id="PotExp"
           className="PotExp h-2 w-[7vw] mt-5"
           max="100"
-          value="70"
-        />
+          value={expLevel.exp}
+        />}
       </div>
       <div
         id="PotDescrption"
@@ -27,11 +36,11 @@ export default function PotCard({dday}) {
           알콩달콩 <span className="text-cherry">커플</span>
         </p>
         <div id="PotCoupleName" className="flex flex-row items-center">
-          <p className="text-[1.2vw] text-text-black">나희도</p>
+          <p className="text-[1.2vw] text-text-black">{userInfos[0].nickname}</p>
           <div>
             <img src={HeartIcon} alt="HeartIcon" />
           </div>
-          <p className="text-[1.2vw] text-text-black">백이진</p>
+          <p className="text-[1.2vw] text-text-black">{userInfos[1].nickname}</p>
         </div>
         <p className="text-[1.2vw] text-text-black">D + {dday}</p>
         <button id="GoPot" className="text-[1.2vw] text-text-black">
