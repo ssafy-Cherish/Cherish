8c7db55b14e554add2126f5efa6ee7c652245f6b
전찬혁 <cksgur5530@gmail.com>
2024-02-11 22:30:39 +0900

    Fix : question.content null 체크
    이슈 코드 : []

--- frontend/cherish/src/components/Main/TodayQuestionCard.jsx
+++ frontend/cherish/src/components/Main/TodayQuestionCard.jsx
@@ -1,26 +1,19 @@
 import MainBookmarkIcon from "../../assets/Main/MainBookmarkIcon.svg";
 import useCoupleStore from "../../stores/useCoupleStore";
 
-
-
 export default function TodayQuestionCard() {
-  const {question} = useCoupleStore()
-  return (
-    <>
-      <div
-        className="flex relative ml-[5vw] w-[40vw] h-[7vw]"
-      >
-        <div className="bg-pink w-[36vw] h-[5vw] rounded-[20px] flex items-center">
-          <p className="text-[1.25vw] font-bold text-text-gray ml-[2vw]">오늘의 질문?</p>
-          <p className="text-[1vw] text-text-black ml-[2vw]">
-          {question.content}
-          </p>
-
-        </div>
-        <div className="absolute left-[80%] -top-[20%]">
-          <img src={MainBookmarkIcon} alt="" />
-        </div>
-      </div>
-    </>
-  );
+	const { question } = useCoupleStore();
+	return (
+		<>
+			<div className="flex relative ml-[5vw] w-[40vw] h-[7vw]">
+				<div className="bg-pink w-[36vw] h-[5vw] rounded-[20px] flex items-center">
+					<p className="text-[1.25vw] font-bold text-text-gray ml-[2vw]">오늘의 질문?</p>
+					<p className="text-[1vw] text-text-black ml-[2vw]">{question?.content}</p>
+				</div>
+				<div className="absolute left-[80%] -top-[20%]">
+					<img src={MainBookmarkIcon} alt="" />
+				</div>
+			</div>
+		</>
+	);
 }
