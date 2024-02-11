9a1f2cd102e767973824b4ddeae0a368bf584c8f
전찬혁 <cksgur5530@gmail.com>
2024-02-11 22:36:00 +0900

    Fix : exp null 체크
    이슈 코드 : []

--- frontend/cherish/src/components/Main/PotGraph.jsx
+++ frontend/cherish/src/components/Main/PotGraph.jsx
@@ -3,28 +3,25 @@ import { fetchExpLevel } from "../../services/PotService";
 import useCoupleStore from "../../stores/useCoupleStore";
 
 export default function PotGraph() {
-  const { coupleId } = useCoupleStore();
-  const { data: expLevel, isLoading } = useQuery({
-    queryKey: ["expLevel", coupleId],
-    queryFn: () => fetchExpLevel(coupleId),
-  });
-  if (isLoading) {
-    return <div>로딩중</div>;
-  }
+	const { coupleId } = useCoupleStore();
+	const { data: expLevel, isLoading } = useQuery({
+		queryKey: ["expLevel", coupleId],
+		queryFn: () => fetchExpLevel(coupleId),
+	});
+	if (isLoading) {
+		return <div>로딩중</div>;
+	}
 
-  return (
-    <div
-      id="Graph"
-      className="row-start-8 row-end-12 flex justify-center items-center"
-    >
-      <div
-        className="radial-progress text-cherry mt-[3vw] flex flex-col justify-center items-center"
-        style={{ "--value": expLevel.exp, "--size": "12vw" }}
-        role="progressbar"
-      >
-        <p className="text-[1.3vw] text-text-black font-bold">현재 성장치</p>
-        <p className="text-[1.5vw] font-bold">{expLevel.exp}%</p>
-      </div>
-    </div>
-  );
+	return (
+		<div id="Graph" className="row-start-8 row-end-12 flex justify-center items-center">
+			<div
+				className="radial-progress text-cherry mt-[3vw] flex flex-col justify-center items-center"
+				style={{ "--value": expLevel?.exp, "--size": "12vw" }}
+				role="progressbar"
+			>
+				<p className="text-[1.3vw] text-text-black font-bold">현재 성장치</p>
+				<p className="text-[1.5vw] font-bold">{expLevel?.exp}%</p>
+			</div>
+		</div>
+	);
 }
