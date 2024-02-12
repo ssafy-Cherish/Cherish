e01d8d5e473f429bf86885a022cfe97721817acf
전찬혁 <cksgur5530@gmail.com>
2024-02-13 00:15:52 +0900

    Feat : 에러페이지 추가 및 버그 수정
    이슈 코드 : []
    
    - 에러페이지 만들어서 ErrorElement 넣어놨습니다.
    - 일별 미팅 조회에서 collection notNullColumn 추가
    - 프로트 메인 css #root height : 100vh
    - main.jsx 라우터에 errorElement 추가
    - 커플이미지 assets 이미지 업스케일링
    - 로그인할때 주는 데이터에서 userInfos가 항상 size 2가 되게 조절
    - 기타 자잘한 오류 수정

--- frontend/cherish/src/pages/MainPages/Index.jsx
+++ frontend/cherish/src/pages/MainPages/Index.jsx
@@ -10,59 +10,56 @@ import useCoupleStore from "../../stores/useCoupleStore";
 import MainMemo from "../../components/Main/MainMemo";
 
 export default function Index() {
-  const { anniversary } = useCoupleStore();
-  const today = dayjs();
-  const anni = dayjs(anniversary);
-  const rs = today.diff(anni, "day", true);
-  const dday = Math.floor(rs) + 1;
+	const { anniversary } = useCoupleStore();
+	const today = dayjs();
+	const anni = dayjs(anniversary);
+	const rs = today.diff(anni, "day", true);
+	const dday = Math.floor(rs) + 1;
 
-  return (
-    <div className="mt-[4vw]">
-      <div className="flex justify-between">
-        <p className="text-[2.5vw] text-text-gray font-bold">
-          우리 만난지 <span className="text-cherry">{dday}</span>일 째!
-        </p>
-        <Link to="today" id="today">
-          <TodayQuestionCard />
-        </Link>
-      </div>
-      <Carousel />
-      <div
-        id="LinkCradWrapper"
-        className="mt-5 h-[17vw] grid grid-cols-9 gap-5"
-      >
-        <Link
-          to="pot"
-          id="Pot"
-          className="bg-pink col-span-3 grid grid-cols-5 rounded-[20px] shadow-md"
-        >
-          <PotCard dday={dday} />
-        </Link>
-        <Link
-          to="diary"
-          id="Diary"
-          className="bg-skyblue col-span-2  flex flex-col items-center rounded-[20px] shadow-md"
-        >
-          <DiaryCard />
-        </Link>
-        <Link
-          to="cherrycall"
-          id="CherryCall"
-          className="bg-beige col-span-2 flex flex-col items-center justify-around rounded-[20px] shadow-md"
-        >
-          <p className="text-[1.2vw] mt-[1vw] font-bold text-text-gray">
-            <span className="text-cherry">체리콜</span>로 화분을 키워요!
-          </p>
-          <div>
-            <img src={CoupleImg1} alt="CoupleImg1" />
-          </div>
-          <p className="text-[1.2vw] mb-[1.8vw] text-text-black">체리콜</p>
-        </Link>
-        <div id="Memo" className="col-span-2">
-          <MainMemo />
-        </div>
-      </div>
-      <Outlet />
-    </div>
-  );
+	return (
+		<div className="mt-[4vw]">
+			<div className="flex justify-between">
+				<p className="text-[2.5vw] text-text-gray font-bold">
+					우리 만난지 <span className="text-cherry">{dday}</span>일 째!
+				</p>
+				<Link to="today" id="today">
+					<TodayQuestionCard />
+				</Link>
+			</div>
+			<Carousel />
+			<div id="LinkCradWrapper" className="mt-5 h-[17vw] grid grid-cols-9 gap-5">
+				<Link
+					to="pot"
+					id="Pot"
+					className="bg-pink col-span-3 grid grid-cols-5 rounded-[20px] shadow-md"
+				>
+					<PotCard dday={dday} />
+				</Link>
+				<Link
+					to="diary"
+					id="Diary"
+					className="bg-skyblue col-span-2  flex flex-col items-center rounded-[20px] shadow-md"
+				>
+					<DiaryCard />
+				</Link>
+				<Link
+					to="cherrycall"
+					id="CherryCall"
+					className="bg-beige col-span-2 flex flex-col items-center justify-around rounded-[20px] shadow-md"
+				>
+					<p className="text-[1.2vw] mt-[1vw] font-bold text-text-gray">
+						<span className="text-cherry">체리콜</span>로 화분을 키워요!
+					</p>
+					<div className="grow flex justify-center items-center w-[70%]">
+						<img src={CoupleImg1} alt="CoupleImg1" />
+					</div>
+					<p className="text-[1.2vw] mb-[1.8vw] text-text-black">체리콜</p>
+				</Link>
+				<div id="Memo" className="col-span-2">
+					<MainMemo />
+				</div>
+			</div>
+			<Outlet />
+		</div>
+	);
 }
