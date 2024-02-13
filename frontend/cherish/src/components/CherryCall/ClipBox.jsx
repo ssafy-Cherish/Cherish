420a52a811265e57042d7834a1e1cfb06f3da664
yangwonseok98 <diddnjstjr11@naver.com>
2024-02-13 20:32:17 +0900

    Feat: clip keyword 추가
    - clipBox에 keyword를 추가로 삽입
    
    이슈 코드 : []

--- frontend/cherish/src/components/CherryCall/ClipBox.jsx
+++ frontend/cherish/src/components/CherryCall/ClipBox.jsx
@@ -1,52 +1,31 @@
 function ClipBox({ meetingInfo, clipWindow }) {
-  return (
-    <div className="h-[80%] flex flex-col mx-4 justify-between relative">
-      <div
-        className="scroll-box bg-white  rounded-2xl h-[100%] w-full overflow-y-scroll py-[5%] absolute"
-        ref={clipWindow}
-      >
-        {meetingInfo.clipHistory.map((url, idx) => {
-          return (
-            <div key={idx} className="flex flex-col items-center my-4">
-              {
-                /* <div className="flex flex-row justify-evenly">
-                  <div className="w-[50%]">
-                    <video
-                      src={URL.createObjectURL(clip[0])}
-                      onClick={(event) => {
-                        event.preventDefault();
-                        event.target.play();
-                      }}
-                    ></video>
-                  </div>
-                  <div className="w-[50%]">
-                    <video
-                      src={URL.createObjectURL(clip[1])}
-                      onClick={(event) => {
-                        event.preventDefault();
-                        event.target.play();
-                      }}
-                    ></video>
-                  </div>
-                </div> */
-                <video
-                  src={url}
-                  onClick={(event) => {
-                    event.preventDefault();
-                    if (event.target.paused) {
-                      event.target.play();
-                    } else {
-                      event.target.pause();
-                    }
-                  }}
-                ></video>
-              }
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-[80%] flex flex-col mx-4 justify-between relative">
+			<div
+				className="scroll-box bg-white  rounded-2xl h-[100%] w-full overflow-y-scroll py-[5%] absolute"
+				ref={clipWindow}
+			>
+				{meetingInfo.clipHistory.map((clip, idx) => {
+					return (
+						<div key={idx} className="flex flex-col items-center my-4">
+							<div>{clip.keyword}</div>
+							<video
+								src={clip.url}
+								onClick={(event) => {
+									event.preventDefault();
+									if (event.target.paused) {
+										event.target.play();
+									} else {
+										event.target.pause();
+									}
+								}}
+							></video>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default ClipBox;
