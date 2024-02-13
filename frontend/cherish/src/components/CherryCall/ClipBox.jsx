73a49e1f5da2e3b1d4af3675b1e7ed03395aea52
양원석 <diddnjstjr11@naver.com>
2024-02-13 16:09:04 +0900

    Fix: 클립박스 초기설정
    - 클립박스 스크롤 테스트 후 수정
    
    이슈 코드 : []

--- frontend/cherish/src/components/CherryCall/ClipBox.jsx
+++ frontend/cherish/src/components/CherryCall/ClipBox.jsx
@@ -1,8 +1,8 @@
 function ClipBox({ meetingInfo, clipWindow }) {
   return (
-    <div className="h-[80%] flex flex-col justify-between relative">
+    <div className="h-[80%] flex flex-col mx-4 justify-between relative">
       <div
-        className="scroll-box bg-white mx-4 rounded-2xl h-[100%] overflow-y-scroll py-[5%] absolute"
+        className="scroll-box bg-white  rounded-2xl h-[100%] w-full overflow-y-scroll py-[5%] absolute"
         ref={clipWindow}
       >
         {meetingInfo.clipHistory.map((url, idx) => {
