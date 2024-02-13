c506ba8e212ce7c4bffe0c2a6f5fc92aedd83f8d
전찬혁 <cksgur5530@gmail.com>
2024-02-14 08:48:08 +0900

    Feat : 카카오 로그인 만료 확인
    이슈 코드 : [S10P12D103-277]
    
    - 메인 to 로그인  리다이렉트 변경
    - 기념일 애니메이션 추가

--- frontend/cherish/src/pages/RootLayout.jsx
+++ frontend/cherish/src/pages/RootLayout.jsx
@@ -1,22 +1,39 @@
 import { Outlet, Navigate } from "react-router-dom";
 import NavBar from "../components/Common/NavBar";
 import useUserStore from "../stores/useUserStore";
+import Kakao from "../utils/Kakao";
+import useCoupleStore from "../stores/useCoupleStore";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-	const { userId } = useUserStore();
+  const { userId } = useUserStore();
 
-	return (
-		<>
-			{userId ? (
-				<div className="grid grid-cols-mainLayout gap-5">
-					<NavBar />
-					<div className="col-span-10 mx-5">
-						<Outlet />
-					</div>
-				</div>
-			) : (
-				<Navigate to="/user" replace />
-			)}
-		</>
-	);
+  const { reset: coupleReset } = useCoupleStore();
+  const { reset: userReset } = useUserStore();
+
+  useEffect(() => {
+    Kakao.Auth.getStatusInfo(({ status }) => {
+      if (status == "not_connected") {
+        coupleReset();
+        userReset();
+        localStorage.removeItem("user-store");
+        localStorage.removeItem("couple-store");
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      {userId ? (
+        <div className="grid grid-cols-mainLayout gap-5">
+          <NavBar />
+          <div className="col-span-10 mx-5">
+            <Outlet />
+          </div>
+        </div>
+      ) : (
+        <Navigate to="/user" replace />
+      )}
+    </>
+  );
 }
