25363bd49340a0bb3a80c525384b7d372bbc0232
전찬혁 <cksgur5530@gmail.com>
2024-02-14 23:48:00 +0900

    Feat : 로그인, 회원가입 반응형
    이슈 코드 : [S10P12D103-277]
    
    - 로그인과 회원가입 페이지에서 페이지 크기가 줄어들면 왼쪽 컴포넌트를
      hidden으로 만들어서 반응형 추가

--- frontend/cherish/src/pages/UserPages/UserLayout.jsx
+++ frontend/cherish/src/pages/UserPages/UserLayout.jsx
@@ -3,24 +3,24 @@ import Intro from "../../components/User/Intro";
 import useUserStore from "../../stores/useUserStore";
 
 const UserLayout = () => {
-  const { userId } = useUserStore();
+	const { userId } = useUserStore();
 
-  return (
-    <>
-      {userId ? (
-        <Navigate to="/" replace />
-      ) : (
-        <div className="grid grid-cols-8 gap-5 h-[100vh]">
-          <div className="col-span-5">
-            <Intro></Intro>
-          </div>
-          <div className="col-span-3">
-            <Outlet></Outlet>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{userId ? (
+				<Navigate to="/" replace />
+			) : (
+				<div className="grid grid-cols-8 gap-5 h-[100vh]">
+					<div className="hidden md:block col-span-5">
+						<Intro></Intro>
+					</div>
+					<div className="col-span-8 md:col-span-3 ">
+						<Outlet></Outlet>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default UserLayout;
