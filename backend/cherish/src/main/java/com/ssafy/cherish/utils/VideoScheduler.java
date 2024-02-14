faabe6700ac9ea468717b4e406e2ed4e1cbb7f3f
김예현 <qkdo12@naver.com>
2024-02-14 09:17:36 +0900

    Fix : 체리보관함 영상 isSelected false라도 조회
    이슈 코드 : []

--- backend/cherish/src/main/java/com/ssafy/cherish/utils/VideoScheduler.java
+++ backend/cherish/src/main/java/com/ssafy/cherish/utils/VideoScheduler.java
@@ -48,8 +48,9 @@ public class VideoScheduler {
     private String monthlyVideoPath;
 
 //     @Scheduled(cron = "0/10 * * * * *") // 10초마다 실행 (테스트용)
-    @Scheduled(cron="0 0 0 * * *")//매일 0시 0분 실행
+
 //    @Scheduled(cron = "0 0 1 1 * *") // 매달 1일 새벽 1시에 실행
+    @Scheduled(cron="0 30 0/1 * * *")//매일 1시간마다 정각에 실행
     public void saveMonthlyVideo() {
         // 모음집 기준 연월 = 이전 달
         Calendar c = Calendar.getInstance();
